b819c17623a93659d25c94f2f919dfee695d2115
Alex W. Lulin <lulinalex@gmail.com>
2014-07-26 13:48:22 +0400

    - заливаем круги цветом.

--- msCircle.pas
+++ msCircle.pas
@@ -6,13 +6,15 @@ uses
  msShape,
  System.Types,
  FMX.Graphics,
- FMX.Types
+ FMX.Types,
+ System.UITypes
  ;
 
 type
  TmsCircle = class(TmsShape)
  protected
   class function Radius: Integer; virtual;
+  function FillColor: TAlphaColor; override;
   procedure DoDrawTo(const aCanvas : TCanvas; const aOrigin : TPointF); override;
  public
   constructor Create(const aStartPoint, aFinishPoint: TPointF); override;
@@ -27,6 +29,11 @@ begin
  Result := 50;
 end;
 
+function TmsCircle.FillColor: TAlphaColor;
+begin
+ Result := TAlphaColorRec.Red;
+end;
+
 constructor TmsCircle.Create(const aStartPoint, aFinishPoint: TPointF);
 begin
  inherited Create(TPointF.Create(aStartPoint.X - Radius, aStartPoint.Y - Radius),
@@ -36,7 +43,11 @@ end;
 procedure TmsCircle.DoDrawTo(const aCanvas: TCanvas; const aOrigin : TPointF);
 begin
  aCanvas.DrawEllipse(TRectF.Create(StartPoint.Add(aOrigin),
-                                   FinishPoint.Add(aOrigin)), 1);
+                                   FinishPoint.Add(aOrigin)),
+                                   1);
+ aCanvas.FillEllipse(TRectF.Create(StartPoint.Add(aOrigin),
+                                   FinishPoint.Add(aOrigin)),
+                                   1);
 end;
 
 initialization
