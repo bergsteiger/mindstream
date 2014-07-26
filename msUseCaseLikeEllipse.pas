fd3b24c46d90f10a3c25d3901d8fde1746b84ecd
Alex W. Lulin <lulinalex@gmail.com>
2014-07-26 14:05:13 +0400

    - добавляем ещё один примитив. Оцените мощь Dependency Injection.

---
+++ msUseCaseLikeEllipse.pas
@@ -0,0 +1,32 @@
+unit msUseCaseLikeEllipse;
+
+interface
+
+uses
+ msCircle,
+ System.UITypes
+ ;
+
+type
+ TmsUseCaseLikeEllipse = class(TmsCircle)
+ protected
+  class function InitialRadiusY: Integer; override;
+  function FillColor: TAlphaColor; override;
+ end;//TmsUseCaseLikeEllipse
+
+implementation
+
+class function TmsUseCaseLikeEllipse.InitialRadiusY: Integer;
+begin
+ Result := 40;
+end;
+
+function TmsUseCaseLikeEllipse.FillColor: TAlphaColor;
+begin
+ Result := TAlphaColorRec.Yellow;
+end;
+
+initialization
+ TmsUseCaseLikeEllipse.Register;
+
+end.
