06c909e3e911aebae050ce4f9544f9cee5787a9a
Alex W. Lulin <lulinalex@gmail.com>
2014-07-26 12:44:22 +0400

    - пользуемся плодами виртуализации.

--- msRoundedRectange.pas
+++ msRoundedRectange.pas
@@ -13,6 +13,8 @@ type
  TmsRoundedRectangle = class(TmsRectangle)
  protected
   class function CornerRadius: Single; override;
+  class function InitialWidth: Single; override;
+  class function InitialHeight: Single; override;
  end;//TmsRoundedRectangle
 
 implementation
@@ -20,6 +22,16 @@ implementation
 class function TmsRoundedRectangle.CornerRadius: Single;
 begin
  Result := 10;
+end;
+
+class function TmsRoundedRectangle.InitialWidth: Single;
+begin
+ Result := 150;
+end;
+
+class function TmsRoundedRectangle.InitialHeight: Single;
+begin
+ Result := InitialWidth;
 end;
 
 initialization
