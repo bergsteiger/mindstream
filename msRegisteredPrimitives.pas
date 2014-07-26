c7f5ee221d43d6710bfd7d544a292cf37ef35241
Alex W. Lulin <lulinalex@gmail.com>
2014-07-26 11:50:47 +0400

    - не надо НИЧЕГО "проектировать на будущее", жизнь обычно - другая.

--- msRegisteredPrimitives.pas
+++ msRegisteredPrimitives.pas
@@ -15,17 +15,10 @@ type
   FmsRegistered : TmsRegistered;
   class var FInstance: TmsRegisteredPrimitives;
   constructor Create;
-  function GetPrimitivesCount: integer;
-  function GetPrimitive(Index: Integer): RmsShape;
-  procedure SetPrimitive(Index: Integer; const aValue: RmsShape);
  public
   class function GetInstance: TmsRegisteredPrimitives;
   procedure AddPrimitive(const aValue : RmsShape);
-  procedure InsertPrimitive(Index: Integer; const aValue: RmsShape);
-  procedure DeletePrimitive(Index: Integer);
-  procedure ClearPrimitives;
-  property PrimitivesCount: Integer read GetPrimitivesCount;
-  property Primitives[Index: Integer]: RmsShape read GetPrimitive write SetPrimitive;
+  property Primitives: TmsRegistered read FmsRegistered;
  end;
 
 implementation
@@ -35,55 +28,20 @@ begin
  FmsRegistered.Add(aValue);
 end;
 
-procedure TmsRegisteredPrimitives.ClearPrimitives;
-begin
- FmsRegistered.Clear;
-end;
-
 constructor TmsRegisteredPrimitives.Create;
 begin
  inherited;
- FmsRegistered := TmsRegistered.Create();
-end;
-
-procedure TmsRegisteredPrimitives.DeletePrimitive(Index: Integer);
-begin
- FmsRegistered.Delete(Index);
+ FmsRegistered := TmsRegistered.Create;
 end;
 
 class function TmsRegisteredPrimitives.GetInstance: TmsRegisteredPrimitives;
 begin
- If FInstance = nil Then
- begin
-  FInstance := TmsRegisteredPrimitives.Create();
- end;
+ if (FInstance = nil) then
+  FInstance := TmsRegisteredPrimitives.Create;
  Result := FInstance;
 end;
 
-function TmsRegisteredPrimitives.GetPrimitive(Index: Integer): RmsShape;
-begin
- Result := FmsRegistered[Index];
-end;
-
-function TmsRegisteredPrimitives.GetPrimitivesCount: integer;
-begin
- Result := FmsRegistered.Count;
-end;
-
-procedure TmsRegisteredPrimitives.InsertPrimitive(Index: Integer;
-  const aValue: RmsShape);
-begin
- FmsRegistered.Insert(Index, aValue);
-end;
-
-procedure TmsRegisteredPrimitives.SetPrimitive(Index: Integer;
-  const aValue: RmsShape);
-begin
- FmsRegistered[Index] := aValue;
-end;
-
 initialization
-
  TmsRegisteredPrimitives.GetInstance.AddPrimitive(TmsLine);
  TmsRegisteredPrimitives.GetInstance.AddPrimitive(TmsRectangle);
  TmsRegisteredPrimitives.GetInstance.AddPrimitive(TmsCircle);
