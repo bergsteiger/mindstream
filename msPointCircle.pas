0c9816fa9f2a4c0643d912c8f26f3fda13208f54
Alex W. Lulin <lulinalex@gmail.com>
2014-07-26 11:19:07 +0400

    - стоит уже определиться - конструкторы мы используем или присвоение по полям.

--- msPointCircle.pas
+++ msPointCircle.pas
@@ -27,11 +27,11 @@ procedure TmsPointCircle.DrawTo(const aCanvas: TCanvas;
 var
  l_StartPoint, l_FinalPoint: TPointF;
 begin
- l_StartPoint.X := StartPoint.X - c_CircleRadius;
- l_StartPoint.Y := StartPoint.Y - c_CircleRadius;
+ l_StartPoint := TPointF.Create(StartPoint.X - c_CircleRadius,
+                                StartPoint.Y - c_CircleRadius);
 
- l_FinalPoint.X := StartPoint.X + c_CircleRadius;
- l_FinalPoint.Y := StartPoint.Y + c_CircleRadius;
+ l_FinalPoint := TPointF.Create(StartPoint.X + c_CircleRadius,
+                                StartPoint.Y + c_CircleRadius);
 
  aCanvas.DrawEllipse(TRectF.Create(l_StartPoint.Add(aOrigin),
                                    l_FinalPoint.Add(aOrigin)), 1);
