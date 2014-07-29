29bd09c8b38ab3d54c304410242944a2e2eb1fbb
Alex W. Lulin <lulinalex@gmail.com>
2014-07-30 00:09:32 +0400

    - виртуализируем.

--- msTriangle.pas
+++ msTriangle.pas
@@ -15,6 +15,7 @@ type
  TmsTriangle = class(TmsShape)
  protected
   class function InitialHeight: Single; virtual;
+  function Polygon: TPolygon; virtual;
   function FillColor: TAlphaColor; override;
   procedure DoDrawTo(const aCanvas : TCanvas; const aOrigin : TPointF); override;
  end;//TmsTriangle
@@ -35,18 +36,23 @@ begin
  Result := 100;
 end;
 
+function TmsTriangle.Polygon: TPolygon;
+begin
+ SetLength(Result, 4);
+ Result[0] := TPointF.Create(StartPoint.X - InitialHeight / 2,
+                        StartPoint.Y + InitialHeight / 2);
+ Result[1] := TPointF.Create(StartPoint.X + InitialHeight / 2,
+                        StartPoint.Y + InitialHeight / 2);
+ Result[2] := TPointF.Create(StartPoint.X,
+                        StartPoint.Y - InitialHeight / 2);
+ Result[3] := Result[0];
+end;
+
 procedure TmsTriangle.DoDrawTo(const aCanvas: TCanvas; const aOrigin : TPointF);
 var
  l_P : TPolygon;
 begin
- SetLength(l_P, 4);
- l_P[0] := TPointF.Create(StartPoint.X - round(InitialHeight) div 2,
-                        StartPoint.Y + round(InitialHeight) div 2);
- l_P[1] := TPointF.Create(StartPoint.X + round(InitialHeight) div 2,
-                        StartPoint.Y + round(InitialHeight) div 2);
- l_P[2] := TPointF.Create(StartPoint.X,
-                        StartPoint.Y - round(InitialHeight) div 2);
- l_P[3] := l_P[0];
+ l_P := Polygon;
  aCanvas.DrawPolygon(l_P, 1);
  aCanvas.FillPolygon(l_P, 0.5);
 end;
