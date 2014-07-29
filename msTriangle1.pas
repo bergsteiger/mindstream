c93621030557de37f2f53f23e6a29fc09fd2baa3
Alex W. Lulin <lulinalex@gmail.com>
2014-07-30 00:27:05 +0400

    - добавляем "ещё один треугольник".

--- msTriangle1.pas
+++ msTriangle1.pas
@@ -3,15 +3,40 @@ unit msTriangle1;
 interface
 
 uses
- msTriangle
+ msTriangle,
+ System.Math.Vectors,
+ System.UITypes
  ;
 
 type
  TmsTriangle1 = class(TmsTriangle)
-
+ protected
+  function Polygon: TPolygon; override;
+  function FillColor: TAlphaColor; override;
  end;//TmsTriangle1
 
 implementation
+
+uses
+ System.Types
+ ;
+
+function TmsTriangle1.FillColor: TAlphaColor;
+begin
+ Result := TAlphaColorRec.Coral;
+end;
+
+function TmsTriangle1.Polygon: TPolygon;
+begin
+ SetLength(Result, 4);
+ Result[0] := TPointF.Create(StartPoint.X - InitialHeight / 2,
+                        StartPoint.Y - InitialHeight / 2);
+ Result[1] := TPointF.Create(StartPoint.X - InitialHeight / 2,
+                        StartPoint.Y + InitialHeight / 2);
+ Result[2] := TPointF.Create(StartPoint.X + InitialHeight / 2,
+                        StartPoint.Y);
+ Result[3] := Result[0];
+end;
 
 initialization
  TmsTriangle1.Register;
