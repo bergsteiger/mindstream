31fa674c72af76b7ea551c1113f189faf481467f
Alex W. Lulin <lulinalex@gmail.com>
2014-08-04 01:46:11 +0400

    - чистим код.

--- msMover.pas
+++ msMover.pas
@@ -15,10 +15,6 @@ type
   f_Moving : TmsShape;
  protected
   procedure DoDrawTo(const aCtx: TmsDrawContext); override;
-(*  function StrokeDash: TStrokeDash; override;
-  function FillColor: TAlphaColor; override;
-  function StrokeColor: TAlphaColor; override;
-  function StrokeThickness: Single; override;*)
  public
   constructor Create(const aStartPoint: TPointF; aListWithOtherShapes: TmsShapeList); override;
   class function IsNeedsSecondClick : Boolean; override;
@@ -67,26 +63,6 @@ begin
 
  //DoDrawDebugInfo(l_Ctx.rCanvas, ClassName);
 end;
-
-(*function TmsMover.FillColor: TAlphaColor;
-begin
- Result := TAlphaColorRec.Darkred;
-end;
-
-function TmsMover.StrokeColor: TAlphaColor;
-begin
- Result := TAlphaColors.Darkmagenta;
-end;
-
-function TmsMover.StrokeDash: TStrokeDash;
-begin
- Result :=  TStrokeDash.sdDashDot;
-end;
-
-function TmsMover.StrokeThickness: Single;
-begin
- Result := 4;
-end;*)
 
 initialization
  TmsMover.Register;
