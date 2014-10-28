120cff038bebc913d23ce4e9ecfe2e35d6f7c986
Alex W. Lulin <lulinalex@gmail.com>
2014-10-29 00:33:46 +0300

    - добавляем "модульные тесты".

--- MindStream.dpr
+++ MindStream.dpr
@@ -25,10 +25,8 @@ uses
   msMover in 'msMover.pas',
   u_fmGUITestRunner in 'FMX.DUnit\u_fmGUITestRunner.pas' {fmGUITestRunner},
   FirstTest in 'Tests\Module\FirstTest.pas',
-  // - тут "банальные" модульные тесты (https://ru.wikipedia.org/wiki/%D0%9C%D0%BE%D0%B4%D1%83%D0%BB%D1%8C%D0%BD%D0%BE%D0%B5_%D1%82%D0%B5%D1%81%D1%82%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D0%B5)
-  RegisteredShapesTest in 'Tests\Integrated\RegisteredShapesTest.pas'
-  // - тут "банальные" интеграционные тесты (https://ru.wikipedia.org/wiki/%D0%98%D0%BD%D1%82%D0%B5%D0%B3%D1%80%D0%B0%D1%86%D0%B8%D0%BE%D0%BD%D0%BD%D0%BE%D0%B5_%D1%82%D0%B5%D1%81%D1%82%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D0%B5)
-  ;
+  RegisteredShapesTest in 'Tests\Integrated\RegisteredShapesTest.pas' {/ - тут "банальные" интеграционные тесты (https: /ru.wikipedia.org/wiki/%D0%98%D0%BD%D1%82%D0%B5%D0%B3%D1%80%D0%B0%D1%86%D0%B8%D0%BE%D0%BD%D0%BD%D0%BE%D0%B5_%D1%82%D0%B5%D1%81%D1%82%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D0%B5)},
+  TestMsLine in 'Tests\Module\TestMsLine.pas';
 
 {$R *.res}
 
