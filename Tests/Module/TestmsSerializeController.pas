f832d2595f825d54b4d1a6a6f54649988452e826
Alex W. Lulin <lulinalex@gmail.com>
2014-12-06 02:30:01 +0300

    - выделяем TmsParametrizedShapeTestSuite в отдельный модуль.

--- Tests/Module/TestmsSerializeController.pas
+++ Tests/Module/TestmsSerializeController.pas
@@ -74,14 +74,6 @@ type
 
   RmsParametrizedShapeTest = class of TmsParametrizedShapeTest;
 
-  TmsParametrizedShapeTestSuite = class(TTestSuite)
-  private
-   constructor CreatePrim;
-  public
-   procedure AddTests(testClass: TTestCaseClass); override;
-   class function Create: ITest;
-  end;//TmsParametrizedShapeTestSuite
-
 implementation
 
  uses
@@ -341,42 +333,5 @@ begin
  f_TestSerializeMethodName := f_Context.rShapeClass.ClassName + '.';
 end;
 
-// TmsParametrizedShapeTestSuite
-
-constructor TmsParametrizedShapeTestSuite.CreatePrim;
-begin
- inherited Create(TmsParametrizedShapeTest);
-end;
-
-class function TmsParametrizedShapeTestSuite.Create: ITest;
-begin
- Result := CreatePrim;
-end;
-
-procedure TmsParametrizedShapeTestSuite.AddTests(testClass: TTestCaseClass);
-begin
- Assert(testClass.InheritsFrom(TmsParametrizedShapeTest));
-
- RandSeed := 10;
- TmsShapeTestPrim.CheckShapes(
-  procedure (aShapeClass: RmsShape)
-  var
-   l_Method: TRttiMethod;
-   l_DiagrammName : String;
-   l_Seed : Integer;
-   l_ShapesCount : Integer;
-  begin
-   l_Seed := Random(High(l_Seed));
-   l_DiagrammName := 'Диаграмма №' + IntToStr(Random(10));
-   l_ShapesCount := Random(1000) + 1;
-   for l_Method in TRttiContext.Create.GetType(testClass).GetMethods do
-    if (l_Method.Visibility = mvPublished) then
-      AddTest(RmsParametrizedShapeTest(testClass).Create(TmsShapeTestContext.Create(l_Method.Name, l_Seed, l_DiagrammName, l_ShapesCount, aShapeClass)));
-  end
- );
-end;
-
-initialization
-  RegisterTest(TmsParametrizedShapeTestSuite.Create);
 end.
 
---
+++ Tests/Module/msParametrizedShapeTestSuite.pas
@@ -0,0 +1,66 @@
+unit msParametrizedShapeTestSuite;
+
+interface
+
+uses
+ TestFramework,
+ msShape
+ ;
+
+type
+  TmsParametrizedShapeTestSuite = class(TTestSuite)
+  private
+   constructor CreatePrim;
+  public
+   procedure AddTests(testClass: TTestCaseClass); override;
+   class function Create: ITest;
+  end;//TmsParametrizedShapeTestSuite
+
+implementation
+
+uses
+ System.TypInfo,
+ System.Rtti,
+ SysUtils,
+ TestmsSerializeController
+ ;
+
+// TmsParametrizedShapeTestSuite
+
+constructor TmsParametrizedShapeTestSuite.CreatePrim;
+begin
+ inherited Create(TmsParametrizedShapeTest);
+end;
+
+class function TmsParametrizedShapeTestSuite.Create: ITest;
+begin
+ Result := CreatePrim;
+end;
+
+procedure TmsParametrizedShapeTestSuite.AddTests(testClass: TTestCaseClass);
+begin
+ Assert(testClass.InheritsFrom(TmsParametrizedShapeTest));
+
+ RandSeed := 10;
+ TmsShapeTestPrim.CheckShapes(
+  procedure (aShapeClass: RmsShape)
+  var
+   l_Method: TRttiMethod;
+   l_DiagrammName : String;
+   l_Seed : Integer;
+   l_ShapesCount : Integer;
+  begin
+   l_Seed := Random(High(l_Seed));
+   l_DiagrammName := 'Диаграмма №' + IntToStr(Random(10));
+   l_ShapesCount := Random(1000) + 1;
+   for l_Method in TRttiContext.Create.GetType(testClass).GetMethods do
+    if (l_Method.Visibility = mvPublished) then
+      AddTest(RmsParametrizedShapeTest(testClass).Create(TmsShapeTestContext.Create(l_Method.Name, l_Seed, l_DiagrammName, l_ShapesCount, aShapeClass)));
+  end
+ );
+end;
+
+initialization
+  RegisterTest(TmsParametrizedShapeTestSuite.Create);
+
+end.
