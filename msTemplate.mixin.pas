56cfb130f6b220e29403fc7e5cae7c826c6e8713
Alex W. Lulin <lulinalex@gmail.com>
2014-12-06 00:25:48 +0300

    - добавляем шаблон примеси.

---
+++ msTemplate.mixin.pas
@@ -0,0 +1,33 @@
+﻿{$IfNDef TmsMixIn_uses_intf}
+
+// interface
+
+{$Define TmsMixIn_uses_intf}
+
+// uses
+
+{$Else TmsMixIn_uses_intf}
+
+{$IfNDef TmsMixIn_intf}
+// http://programmingmindstream.blogspot.ru/2014/12/generic-2.html
+
+{$Define TmsMixIn_intf}
+
+{$Else TmsMixIn_intf}
+
+// implementation
+
+{$IfNDef TmsMixIn_uses_impl}
+
+// uses
+
+{$Define TmsMixIn_uses_impl}
+
+{$Else TmsMixIn_uses_impl}
+
+// TmsTemplate
+
+{$EndIf TmsMixIn_uses_impl}
+
+{$EndIf TmsMixIn_intf}
+{$EndIf TmsMixIn_uses_intf}
