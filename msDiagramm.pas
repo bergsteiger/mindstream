519a1b447f1da5c7de75b53be20fa541e13e5bd1
Alex W. Lulin <lulinalex@gmail.com>
2014-12-08 23:40:08 +0300

    - "бежим впереди паровоза".

--- msDiagramm.pas
+++ msDiagramm.pas
@@ -75,6 +75,10 @@ type
   property CurrentClass: RmsShape read FCurrentClass write FCurrentClass;
  end;//TmsDiagramm
 
+ ImsIvalidator = interface
+  procedure InvalidateDiagramm(aDiagramm: TmsDiagramm);
+ end;//ImsIvalidator
+
 implementation
 
 uses
