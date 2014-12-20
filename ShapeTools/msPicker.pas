6aafd609ed86d03138860c2a0706c2439160f451
Alex W. Lulin <lulinalex@gmail.com>
2014-12-20 03:10:56 +0300

    - начинаем наполнять "скелет" TmsPicker "мясом".

--- ShapeTools/msPicker.pas
+++ ShapeTools/msPicker.pas
@@ -3,14 +3,27 @@ unit msPicker;
 interface
 
 uses
+ msInterfaces,
  msTool
  ;
 
 type
  TmsPicker = class(TmsTool)
   // - утилитный класс для "проваливания" внутрь примитивов
+ public
+  class function Create(const aCtx: TmsMakeShapeContext): ImsShape; override;
  end;//TmsPicker
 
 implementation
+
+class function TmsPicker.Create(const aCtx: TmsMakeShapeContext): ImsShape;
+var
+ l_PickedShape : ImsShape;
+begin
+ l_PickedShape := aCtx.rShapesController.ShapeByPt(aCtx.rStartPoint);
+ if (l_PickedShape = nil) then
+  Exit;
+ Result := inherited Create(aCtx);
+end;
 
 end.
