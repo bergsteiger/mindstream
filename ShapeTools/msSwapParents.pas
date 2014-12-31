494b91b9a77e17d24589e307e2cbcf174a77df61
Alex W. Lulin <lulinalex@gmail.com>
2014-12-31 10:39:45 +0300

    - Добавляем ещё одну стрелку.

--- ShapeTools/msSwapParents.pas
+++ ShapeTools/msSwapParents.pas
@@ -5,7 +5,8 @@ interface
 uses
  msInterfaces,
  msTool,
- msNullClickShape
+ msNullClickShape,
+ System.Types
  ;
 
 type
@@ -13,14 +14,24 @@ type
   // - утилитный класс ОБМЕНА диаграмм МЕСТАМИ
  protected
   class function DoNullClick(const aHolder: ImsDiagrammsHolder): Boolean; override;
+  class function ButtonShape(const aStartPoint: TPointF): ImsShape; override;
  end;//TmsSwapParents
 
 implementation
+
+uses
+ msLineWithArrow
+ ;
 
 class function TmsSwapParents.DoNullClick(const aHolder: ImsDiagrammsHolder): Boolean;
 begin
  Result := true;
  aHolder.SwapParents;
+end;
+
+class function TmsSwapParents.ButtonShape(const aStartPoint: TPointF): ImsShape;
+begin
+ Result := TmsLineWithArrow.CreateCompleted(TPointF.Create(50, 0), TPointF.Create(0, 0));
 end;
 
 end.
