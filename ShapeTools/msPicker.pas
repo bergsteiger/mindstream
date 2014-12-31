0b2b363574a093a7c617dd35d7f4845a043c6e28
Alex W. Lulin <lulinalex@gmail.com>
2014-12-31 10:17:11 +0300

    - рисуем "стрелочки".

--- ShapeTools/msPicker.pas
+++ ShapeTools/msPicker.pas
@@ -5,7 +5,6 @@ interface
 uses
  msInterfaces,
  msTool,
-
  System.Types
  ;
 
--- ShapeTools/msUpToParent.pas
+++ ShapeTools/msUpToParent.pas
@@ -5,7 +5,8 @@ interface
 uses
  msInterfaces,
  msTool,
- msNullClickShape
+ msNullClickShape,
+ System.Types
  ;
 
 type
@@ -13,14 +14,24 @@ type
   // - утилитный класс дл€ "подъЄма" к ѕ–≈ƒџƒ”ў≈… диаграмме
  public
   class function DoNullClick(const aHolder: ImsDiagrammsHolder): Boolean; override;
+  class function ButtonShape(const aStartPoint: TPointF): ImsShape; override;
  end;//TmsUpToParent
 
 implementation
+
+uses
+ msLineWithArrow
+ ;
 
 class function TmsUpToParent.DoNullClick(const aHolder: ImsDiagrammsHolder): Boolean;
 begin
  Result := true;
  aHolder.UpToParent;
+end;
+
+class function TmsUpToParent.ButtonShape(const aStartPoint: TPointF): ImsShape;
+begin
+ Result := TmsLineWithArrow.CreateCompleted(TPointF.Create(0, 50), TPointF.Create(0, 0));
 end;
 
 end.
