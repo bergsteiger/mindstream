8a8a62907b18c332a38723eea52563c3e5d5da54
Alex W. Lulin <lulinalex@gmail.com>
2015-01-11 01:06:24 +0300

    - правим тест.

--- Tests/Integrated/RegisteredShapesTest.pas
+++ Tests/Integrated/RegisteredShapesTest.pas
@@ -36,7 +36,7 @@ begin
    Inc(l_Result);
   end
  );
- CheckTrue(l_Result = 22,  ' Expected 22 - Get ' + IntToStr(l_Result));
+ CheckTrue(l_Result = 23,  ' Expected 23 - Get ' + IntToStr(l_Result));
 end;
 
 procedure TRegisteredShapesTest.TestFirstShape;
