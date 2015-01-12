285eaac25daa915044ebda73d2313a6b5843f577
Alex W. Lulin <lulinalex@gmail.com>
2015-01-13 02:02:15 +0300

    - чистим код.

--- ConcreteShapes/msLineWithArrow.pas
+++ ConcreteShapes/msLineWithArrow.pas
@@ -152,11 +152,6 @@ begin
  l_Angle := GetArrowAngleRotation;
  Result := TPointF.Create(FinishPoint.X - TmsSmallTriangle.InitialHeight * Cos(l_Angle),
                           FinishPoint.Y - TmsSmallTriangle.InitialHeight * Sin(l_Angle));
- // Вот тут, по идее, надо возвращать координату середины стороны,
- //  через которую прходит линия.
- // Сделать я это не смог, потому что не знаю как правильно после поворота треугольника
- // получить новые координаты вершин.
- //Result := inherited GetFinishPointForDraw;
 end;
 
 end.
