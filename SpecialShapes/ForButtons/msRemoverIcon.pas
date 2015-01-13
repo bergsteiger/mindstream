9d425dbe04afd3c85f68288e92fe276ca8d0e20a
Alex W. Lulin <lulinalex@gmail.com>
2015-01-14 02:34:52 +0300

    - примитивно рисуем крестик.

--- SpecialShapes/ForButtons/msRemoverIcon.pas
+++ SpecialShapes/ForButtons/msRemoverIcon.pas
@@ -16,14 +16,19 @@ type
 implementation
 
 uses
- System.Types
+ System.Types,
+ msLine,
+ msShapesGroup
  ;
 
 // TmsRemoverIcon
 
 class function TmsRemoverIcon.Create: ImsShape;
 begin
- Result := inherited Create(TPointF.Create(0, 0));
+ Result := TmsShapesGroup.Create([
+  TmsLine.CreateCompleted(TPointF.Create(0, 0), TPointF.Create(50, 50)),
+  TmsLine.CreateCompleted(TPointF.Create(50, 0), TPointF.Create(0, 50))
+  ]);
 end;
 
 end.
