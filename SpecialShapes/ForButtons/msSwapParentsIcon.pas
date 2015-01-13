039c29823e423ef207f1ad2565c3b176ac753e73
Alex W. Lulin <lulinalex@gmail.com>
2015-01-14 02:24:34 +0300

    - реализуем.

--- SpecialShapes/ForButtons/msSwapParentsIcon.pas
+++ SpecialShapes/ForButtons/msSwapParentsIcon.pas
@@ -8,8 +8,25 @@ uses
 
 type
  TmsSwapParentsIcon = class
+ public
+  class function Create: ImsShape;
  end;//TmsSwapParentsIcon
 
 implementation
+
+uses
+ System.Types,
+ msShapesGroup,
+ msLeftArrow,
+ msLineWithArrow,
+ msSmallTriangle
+ ;
+
+// TmsSwapParentsIcon
+
+class function TmsSwapParentsIcon.Create: ImsShape;
+begin
+ Result := TmsShapesGroup.Create([TmsLeftArrow.Create, TmsLineWithArrow.CreateCompleted(TPointF.Create(TmsSmallTriangle.InitialHeight, 0), TPointF.Create(TmsLeftArrow.InitialLength, 0))]);
+end;
 
 end.
