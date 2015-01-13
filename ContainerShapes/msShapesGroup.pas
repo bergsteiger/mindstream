50980c5d2eb089910b5a87bbc6bd4c3b5efb26da
Alex W. Lulin <lulinalex@gmail.com>
2015-01-14 01:11:16 +0300

    - добавляем класс TmsShapesList.

--- ContainerShapes/msShapesGroup.pas
+++ ContainerShapes/msShapesGroup.pas
@@ -4,10 +4,15 @@ unit msShapesGroup;
 interface
 
 uses
+ Generics.Collections,
+ msInterfaces,
  msShape
  ;
 
 type
+ TmsShapesList = class(TList<ImsShape>)
+ end;//TmsShapesList
+
  TmsShapesGroup = class(TmsShape)
  end;//TmsShapesGroup
 
