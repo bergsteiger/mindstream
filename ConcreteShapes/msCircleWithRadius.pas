ab2699fbb21c0578b12f19ece3413226e20442be
Alex W. Lulin <lulinalex@gmail.com>
2015-01-15 04:11:47 +0300

    - развиваем тему.

--- ConcreteShapes/msCircleWithRadius.pas
+++ ConcreteShapes/msCircleWithRadius.pas
@@ -8,8 +8,17 @@ uses
 
 type
  TmsCircleWithRadius = class(TmsCircle)
+ protected
+  function InitialRadiusX: Integer; override;
  end;//TmsCircleWithRadius
 
 implementation
+
+// TmsCircleWithRadius
+
+function TmsCircleWithRadius.InitialRadiusX: Integer;
+begin
+ Result := inherited;
+end;
 
 end.
