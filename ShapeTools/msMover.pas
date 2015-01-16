f9f5a9b8a84fcf427337cda6a7dbcdb42dd2cb7e
Alex W. Lulin <lulinalex@gmail.com>
2015-01-17 01:37:16 +0300

    - ровняем код.

--- ShapeTools/msMover.pas
+++ ShapeTools/msMover.pas
@@ -64,10 +64,12 @@ begin
  l_B := aButton.DrawBounds;
  l_Mid.X := (l_B.Left + l_B.Right) / 2;
  l_Mid.Y := (l_B.Top + l_B.Bottom) / 2;
- Result := f_FloatingButtons.AddShape(TmsShapesGroup.Create([
-            TmsCircleWithRadius.Create(l_Mid, Max(-(l_B.Left - l_B.Right), -(l_B.Top - l_B.Bottom)) / 2 + cShift / 2 ),
-            aButton
-            ]));
+ Result := f_FloatingButtons.AddShape(
+            TmsShapesGroup.Create([
+             TmsCircleWithRadius.Create(l_Mid, Max(-(l_B.Left - l_B.Right), -(l_B.Top - l_B.Bottom)) / 2 + cShift / 2 ),
+             aButton
+            ])
+           );
 end;
 
 constructor TmsMover.CreateInner(const aStartPoint: TPointF; const aMoving: ImsShape; const aController: ImsShapesController);
