6458c5328f3904ad57c2ae7ca6e460cc5995b7be
Alex W. Lulin <lulinalex@gmail.com>
2015-01-20 23:58:10 +0300

    - Обобщаем классы.

--- AbstractShapes/msSVG_Shape.pas
+++ AbstractShapes/msSVG_Shape.pas
@@ -13,23 +13,11 @@ uses
 
 type
  TmsSVG_Shape = class abstract(TmsPolygonShape)
-// TmsSVG_Shape = class abstract(TmsShape)
-  // - класс для реализации SVG объектов
- protected
-  procedure DoDrawTo(const aCtx: TmsDrawContext); override;
+ // - базовый класс для реализации SVG объектов
  end;//TmsPolygonShape
 
 implementation
 
-// TmsPolygonShape
-
-procedure TmsSVG_Shape.DoDrawTo(const aCtx: TmsDrawContext);
-var
- l_P : TPolygon;
-begin
- l_P := Polygon;
- aCtx.rCanvas.DrawPolygon(l_P, 1);
- aCtx.rCanvas.FillPolygon(l_P, 0.5);
-end;
+// TmsSVG_Shape
 
 end.
