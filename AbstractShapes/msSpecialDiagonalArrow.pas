5fe1611268b1c2ac6345f7df163c657d614059ae
Alex W. Lulin <lulinalex@gmail.com>
2015-01-24 01:24:59 +0300

    - используем виртуальность. Пока как нул-преобразование.

--- AbstractShapes/msSpecialDiagonalArrow.pas
+++ AbstractShapes/msSpecialDiagonalArrow.pas
@@ -8,10 +8,17 @@ uses
 
 type
  TmsSpecialDiagonalArrow = class abstract(TmsSpecialArrow)
+ public
+  class function InitialLength: Integer; override;
  end;//TmsSpecialDiagonalArrow
 
 implementation
 
 // TmsSpecialDiagonalArrow
+
+class function TmsSpecialDiagonalArrow.InitialLength: Integer;
+begin
+ Result := inherited InitialLength;
+end;
 
 end.
