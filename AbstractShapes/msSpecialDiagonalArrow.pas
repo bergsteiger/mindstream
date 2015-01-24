b7ba47eb3df26593952493e812317a6e5863c052
Alex W. Lulin <lulinalex@gmail.com>
2015-01-24 04:41:51 +0300

    - добавляем модуль msCoreUtils.

--- AbstractShapes/msSpecialDiagonalArrow.pas
+++ AbstractShapes/msSpecialDiagonalArrow.pas
@@ -19,14 +19,14 @@ type
 implementation
 
 uses
- Math
+ Math,
+ msCoreUtils
  ;
 
 // TmsSpecialDiagonalArrow
 
 class function TmsSpecialDiagonalArrow.CreateCompletedInternal(const aStartPoint: TPointF; const aFinishPoint: TPointF): ImsShape;
 var
- l_Temp : Extended;
  l_StartPoint: TPointF;
  l_FinishPoint: TPointF;
  l_Diff : TPointF;
@@ -40,17 +40,9 @@ begin
  l_StartPoint := l_Mid - l_Diff;
  l_FinishPoint := l_Mid + l_Diff;
  if (aStartPoint.X > aFinishPoint.X) then
- begin
-  l_Temp := l_StartPoint.X;
-  l_StartPoint.X := l_FinishPoint.X;
-  l_FinishPoint.X := l_Temp;
- end;//aStartPoint.X > aFinishPoint.X
+  msSwap(l_StartPoint.X, l_FinishPoint.X);
  if (aStartPoint.Y > aFinishPoint.Y) then
- begin
-  l_Temp := l_StartPoint.Y;
-  l_StartPoint.Y := l_FinishPoint.Y;
-  l_FinishPoint.Y := l_Temp;
- end;//aStartPoint.Y > aFinishPoint.Y
+  msSwap(l_StartPoint.Y, l_FinishPoint.Y);
  Result := inherited CreateCompletedInternal(l_StartPoint, l_FinishPoint);
 end;
 
