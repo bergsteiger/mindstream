16ca55697250e9b470cbdebfc526c4840b1d0bd0
Ingword <instigator21@gmail.com>
2015-01-26 16:58:31 +0200

    правильно подписываем

--- SpecialShapes/ForButtons/msRemoverIcon.pas
+++ SpecialShapes/ForButtons/msRemoverIcon.pas
@@ -9,10 +9,12 @@ uses
 
 type
  TmsRemoverIcon = class(TmsSVGShape)
- // - "папка"
+ // - "Иконка для удаления фигур"
  protected
   function GetPolygonSVG: String; override;
   procedure TransformDrawOptionsContext(var theCtx: TmsDrawOptionsContext); override;
+ public
+  class function IsForToolbar: Boolean; override;
  end;//TmsFolder
 
 implementation
@@ -27,6 +29,11 @@ function TmsRemoverIcon.GetPolygonSVG: String;
 begin
  Result := 'M 10,30 L 30,10 L 50,30 L 70,10 L 90,30 L 70,50 L 90,70' +
            'L 70,90 L 50,70 L 30,90 L 10,70 L 30,50 L 30,50 L 10,30';
+end;
+
+class function TmsRemoverIcon.IsForToolbar: Boolean;
+begin
+ Result := False;
 end;
 
 procedure TmsRemoverIcon.TransformDrawOptionsContext(
