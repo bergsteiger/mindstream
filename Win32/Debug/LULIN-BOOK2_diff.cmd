9cc9dfd1cfc6b3b211112875601be01ce0bca656
Alex W. Lulin <lulinalex@gmail.com>
2015-01-29 02:45:09 +0300

    - расставляем правильные кавычки.

--- Win32/Debug/LULIN-BOOK2_diff.cmd
+++ Win32/Debug/LULIN-BOOK2_diff.cmd
@@ -1,4 +1,4 @@
 echo "Differing"
-echo %1 
-echo %2
-start "" "C:\Program Files\Araxis\Araxis Merge\Compare.exe"  "%1" "%2"
+echo %~1 
+echo %~2
+start "" "C:\Program Files\Araxis\Araxis Merge\Compare.exe"  "%~1" "%~2"
