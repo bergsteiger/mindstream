2a08dc4813f1785798cdcb7317dd77e05098d747
Alex W. Lulin <lulinalex@gmail.com>
2015-01-29 02:52:08 +0300

    - добавляем интерфейс ImsLog. https://bitbucket.org/ingword/mindstream/issue/78/rundiffprim-rundiff-deleteetalonfile

--- FMX.DUnit/FMX.DUnit.Interfaces.pas
+++ FMX.DUnit/FMX.DUnit.Interfaces.pas
@@ -3,6 +3,9 @@ unit FMX.DUnit.Interfaces;
 interface
 
 type
+ ImsLog = interface
+ end;//ImsLog
+
  ImsEtalonsHolder = interface
  ['{1758A741-7AB3-404C-ADC0-FF6DDD815535}']
   procedure DeleteEtalonFile;
