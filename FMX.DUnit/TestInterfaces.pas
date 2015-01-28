41b97f20541af7f1d7212aa17e552a41d9fbd2b9
Alex W. Lulin <lulinalex@gmail.com>
2015-01-28 23:35:59 +0300

    - чистим код.

--- FMX.DUnit/TestInterfaces.pas
+++ FMX.DUnit/TestInterfaces.pas
@@ -1,12 +1,13 @@
 unit TestInterfaces;
 
 interface
+
 type
  ImsEtalonsHolder = interface
  ['{1758A741-7AB3-404C-ADC0-FF6DDD815535}']
   procedure DeleteEtalonFile;
   procedure RunDiff;
- end;
+ end;//ImsEtalonsHolder
 
 implementation
 
