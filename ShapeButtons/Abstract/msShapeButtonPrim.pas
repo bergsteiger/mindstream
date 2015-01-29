4bce2c42df11209e9332de1652dcf774b88b89a2
Ingword <instigator21@gmail.com>
2015-01-30 00:58:38 +0200

    Делаем заготовку и игнорим хистори

---
+++ ShapeButtons/Abstract/msShapeButtonPrim.pas
@@ -0,0 +1,22 @@
+unit msShapeButtonPrim;
+
+interface
+
+uses
+ msShape,
+ FMX.StdCtrls,
+ FMX.Controls,
+ FMX.Graphics,
+ System.Types,
+ msInterfaces,
+ System.Classes,
+ FMX.ListBox
+ ;
+
+type
+ TmsShapeButtonPrim = class abstract(TButton)
+ end;
+
+implementation
+
+end.
