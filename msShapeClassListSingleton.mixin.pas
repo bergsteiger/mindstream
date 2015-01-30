834de9280c082dd7803d9812854eb2daea96079b
Alex W. Lulin <lulinalex@gmail.com>
2015-01-31 01:28:08 +0300

    - добавляем комментарий.

--- msShapeClassListSingleton.mixin.pas
+++ msShapeClassListSingleton.mixin.pas
@@ -15,6 +15,7 @@
 // http://programmingmindstream.blogspot.ru/2014/12/generic-2.html
 
  TmsShapeClassListSingleton = class(TmsShapeClassList)
+ // [Одиночка (шаблон проектирования)|https://ru.wikipedia.org/wiki/%D0%9E%D0%B4%D0%B8%D0%BD%D0%BE%D1%87%D0%BA%D0%B0_(%D1%88%D0%B0%D0%B1%D0%BB%D0%BE%D0%BD_%D0%BF%D1%80%D0%BE%D0%B5%D0%BA%D1%82%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D1%8F)]
  strict private
   class var f_Instance: TmsShapeClassListSingleton;
   class destructor Fini;
