79c89074751160d68bf839b1ca86c0b2fc8d7b10
Alex W. Lulin <lulinalex@gmail.com>
2015-01-31 01:25:06 +0300

    - добавляем комментарий.

--- ContainerShapes/msShapesGroup.pas
+++ ContainerShapes/msShapesGroup.pas
@@ -14,6 +14,7 @@ uses
 
 type
  TmsShapesGroup = class(TmsPointlessShape)
+ // [Декоратор (шаблон проектирования)|https://ru.wikipedia.org/wiki/%D0%94%D0%B5%D0%BA%D0%BE%D1%80%D0%B0%D1%82%D0%BE%D1%80_(%D1%88%D0%B0%D0%B1%D0%BB%D0%BE%D0%BD_%D0%BF%D1%80%D0%BE%D0%B5%D0%BA%D1%82%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D1%8F)]
  private
   f_Shapes : TmsShapesList;
  protected
