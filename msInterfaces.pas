1a20a3b6d9108c1c96393598d9a1f77628de90de
Alex W. Lulin <lulinalex@gmail.com>
2015-01-31 01:41:39 +0300

    - добавляем комментарий.

--- msInterfaces.pas
+++ msInterfaces.pas
@@ -28,6 +28,7 @@ type
  // А вот с UML - мы его ПОТОМ СГЕНЕРИРУЕМ
 
  TmsDrawContext  = record
+ // [Состояние (шаблон проектирования)|https://ru.wikipedia.org/wiki/%D0%A1%D0%BE%D1%81%D1%82%D0%BE%D1%8F%D0%BD%D0%B8%D0%B5_(%D1%88%D0%B0%D0%B1%D0%BB%D0%BE%D0%BD_%D0%BF%D1%80%D0%BE%D0%B5%D0%BA%D1%82%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D1%8F)]
  // Контекст рисования.
  // "Лирика" - тут - http://habrahabr.ru/post/170125/
  // Ну и "связанное" - https://ru.wikipedia.org/wiki/%D0%A1%D1%82%D1%80%D0%B0%D1%82%D0%B5%D0%B3%D0%B8%D1%8F_(%D1%88%D0%B0%D0%B1%D0%BB%D0%BE%D0%BD_%D0%BF%D1%80%D0%BE%D0%B5%D0%BA%D1%82%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D1%8F)
