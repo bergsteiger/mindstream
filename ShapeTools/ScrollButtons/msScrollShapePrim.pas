c6bdf2e30891dbbd6e59047c33f101b88e7f74b9
Ingword <instigator21@gmail.com>
2015-01-31 03:13:09 +0200

    убираем кнопки с TollBar'а

--- ShapeTools/ScrollButtons/msScrollShapePrim.pas
+++ ShapeTools/ScrollButtons/msScrollShapePrim.pas
@@ -8,8 +8,17 @@ uses
 
 type
  TmsScrollShapePrim = class(TmsNullClickShape)
+ public
+   class function IsForToolbar: Boolean; override;
  end; // TmsScrollShapePrim
 
 implementation
+
+{ TmsScrollShapePrim }
+
+class function TmsScrollShapePrim.IsForToolbar: Boolean;
+begin
+ Result := False;
+end;
 
 end.
