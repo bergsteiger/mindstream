c5e02ef815cf34002e7a650ab1688f05647b617f
Ingword <instigator21@gmail.com>
2015-01-31 03:12:48 +0200

    Делаем красивую иконку сбросу.

--- ShapeTools/ScrollButtons/msScrollShapeResetOrigin.pas
+++ ShapeTools/ScrollButtons/msScrollShapeResetOrigin.pas
@@ -11,14 +11,25 @@ uses
 
 type
  TmsScrollShapeResetOrigin = class(TmsScrollShapePrim)
+ protected
+  class function ButtonShape: ImsShape; override;
  public
   class function DoNullClick(const aHolder: ImsDiagrammsHolder): Boolean; override; final;
  public
  end;//TmsScrollShapeResetOrigin
 
 implementation
-
+uses
+ msShapesGroup,
+ msRemoveIcon,
+ msRectangle
+ ;
 // TmsScrollShapeResetOrigin
+
+class function TmsScrollShapeResetOrigin.ButtonShape: ImsShape;
+begin
+ Result := TmsShapesGroup.Create([TmsRectangle.Create, TmsRemoveIcon.Create]);
+end;
 
 class function TmsScrollShapeResetOrigin.DoNullClick(
   const aHolder: ImsDiagrammsHolder): Boolean;
