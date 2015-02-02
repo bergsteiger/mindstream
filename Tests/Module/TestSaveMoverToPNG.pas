8381ed3c77ab92b5b701675a7b77db81ddaa2ed2
Alex W. Lulin <lulinalex@gmail.com>
2015-02-02 23:36:23 +0300

    - добавляем Mover первой фигуреи видим как изменились эталоны.

--- Tests/Module/TestSaveMoverToPNG.pas
+++ Tests/Module/TestSaveMoverToPNG.pas
@@ -17,13 +17,25 @@ type
 
 implementation
 
+uses
+  msMover,
+  msRegisteredShapes
+  ;
+
 // TTestSaveMoverToPNG
 
 procedure TTestSaveMoverToPNG.SaveDiagramm(const aFileName: String; const aDiagramm: ImsDiagramm);
 var
  l_ShapeToDeal : ImsShape;
+ l_Class : ImsShapeClass;
+ l_Mover : ImsShape;
+ l_Ctx : TmsMakeShapeContext;
 begin
  l_ShapeToDeal := aDiagramm.FirstShape;
+ l_Class := TmsRegisteredShapes.Instance.ByName(TmsMover.ClassName);
+ Assert(l_Class <> nil);
+ l_Ctx := TmsMakeShapeContext.Create(l_ShapeToDeal.StartPoint, aDiagramm.ShapesController, nil);
+ l_Mover := l_Class.Creator.CreateShape(l_Ctx);
  inherited;
 end;
 
