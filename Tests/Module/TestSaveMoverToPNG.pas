6eae34389fdd8738e28e1234b901d52fd7bcec09
Alex W. Lulin <lulinalex@gmail.com>
2015-02-02 23:17:27 +0300

    - Заготовочка.

--- Tests/Module/TestSaveMoverToPNG.pas
+++ Tests/Module/TestSaveMoverToPNG.pas
@@ -11,9 +11,18 @@ uses
 
 type
   TTestSaveMoverToPNG = class(TTestSaveToPNG)
+  protected
+   procedure SaveDiagramm(const aFileName: String; const aDiagramm: ImsDiagramm); override;
   end;//TTestSaveMoverToPNG
 
 implementation
+
+// TTestSaveMoverToPNG
+
+procedure TTestSaveMoverToPNG.SaveDiagramm(const aFileName: String; const aDiagramm: ImsDiagramm);
+begin
+ inherited;
+end;
 
 end.
 
