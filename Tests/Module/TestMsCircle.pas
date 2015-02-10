255066c1464f15beba6743a6cfea0232d4235068
Alex W. Lulin <lulinalex@gmail.com>
2015-02-11 01:55:56 +0300

    - разбираемся с предупреждениями компилятора.

--- Tests/Module/TestMsCircle.pas
+++ Tests/Module/TestMsCircle.pas
@@ -48,13 +48,9 @@ end;
 
 procedure TestTmsCircle.TestContainsPt;
 var
-  ReturnValue: Boolean;
-  aPoint: TPointF;
-  l_HitShape: ImsShape;
+ l_HitShape: ImsShape;
 begin
-  // TODO: Setup method call parameters
-  ReturnValue := FmsCircle.HitTest(aPoint, l_HitShape);
-  // TODO: Validate method results
+ CheckTrue(FmsCircle.HitTest(TPointF.Create(0, 0), l_HitShape));
 end;
 
 initialization
