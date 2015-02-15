b775ebf6cca9378c075cfcd5f567e80d4d326f21
Alex W. Lulin <lulinalex@gmail.com>
2015-02-15 20:06:13 +0300

    - выделяем класс TmsProxyShapePrim.

--- ContainerShapes/msProxyShape.pas
+++ ContainerShapes/msProxyShape.pas
@@ -12,18 +12,22 @@ uses
  ;
 
 type
- TmsProxyShape = class(TmsPointlessShape)
+ TmsProxyShapePrim = class abstract(TmsPointlessShape)
  // [Декоратор (шаблон проектирования)|https://ru.wikipedia.org/wiki/%D0%94%D0%B5%D0%BA%D0%BE%D1%80%D0%B0%D1%82%D0%BE%D1%80_(%D1%88%D0%B0%D0%B1%D0%BB%D0%BE%D0%BD_%D0%BF%D1%80%D0%BE%D0%B5%D0%BA%D1%82%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D1%8F)]
- private
-  f_Shape : ImsShape;
  protected
   procedure DoDrawTo(const aCtx: TmsDrawContext); override;
   function GetDrawBounds: TRectF; override;
   function pm_GetStartPoint: TPointF; override;
   function HitTest(const aPoint: TPointF; out theShape: ImsShape): Boolean; override;
+  function ShapeToShow: ImsShape; virtual; abstract;
+ end;//TmsProxyShapePrim
+
+ TmsProxyShape = class(TmsProxyShapePrim)
+ private
+  f_Shape : ImsShape;
+ protected
+  function ShapeToShow: ImsShape; override;
   constructor CreateInner(const aShape: ImsShape); reintroduce;
-  property ShapeToShow : ImsShape
-   read f_Shape;
  public
   class function Create(const aShape: ImsShape): ImsShape;
   procedure Cleanup; override;
@@ -50,25 +54,32 @@ begin
  f_Shape := aShape;
 end;
 
+function TmsProxyShape.ShapeToShow: ImsShape;
+begin
+ Result := f_Shape;
+end;
+
 procedure TmsProxyShape.Cleanup;
 begin
  f_Shape := nil;
  inherited;
 end;
 
-procedure TmsProxyShape.DoDrawTo(const aCtx: TmsDrawContext);
+// TmsProxyShapePrim
+
+procedure TmsProxyShapePrim.DoDrawTo(const aCtx: TmsDrawContext);
 begin
  Assert(ShapeToShow <> nil);
  ShapeToShow.DrawTo(aCtx);
 end;
 
-function TmsProxyShape.GetDrawBounds: TRectF;
+function TmsProxyShapePrim.GetDrawBounds: TRectF;
 begin
  Assert(ShapeToShow <> nil);
  Result := ShapeToShow.DrawBounds;
 end;
 
-function TmsProxyShape.pm_GetStartPoint: TPointF;
+function TmsProxyShapePrim.pm_GetStartPoint: TPointF;
 var
  l_R : TRectF;
 begin
@@ -76,7 +87,7 @@ begin
  Result := (l_R.TopLeft + l_R.BottomRight) / 2;
 end;
 
-function TmsProxyShape.HitTest(const aPoint: TPointF; out theShape: ImsShape): Boolean;
+function TmsProxyShapePrim.HitTest(const aPoint: TPointF; out theShape: ImsShape): Boolean;
 begin
  Assert(ShapeToShow <> nil);
  Result := ShapeToShow.HitTest(aPoint, theShape);
