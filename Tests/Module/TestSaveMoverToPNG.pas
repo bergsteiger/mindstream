0c1a50929d5b6cd0ac38f6f9c431e6c60be5f0b6
Alex W. Lulin <lulinalex@gmail.com>
2015-02-18 03:14:21 +0300

    - двигаем дельту "туда-сюда".

--- Tests/Module/TestSaveMoverToPNG.pas
+++ Tests/Module/TestSaveMoverToPNG.pas
@@ -132,7 +132,7 @@ end;
 
 procedure TmsConnectorDrawTest.SaveDiagramm(const aFileName: String; const aDiagramm: ImsDiagramm);
 const
- cDelta = 15;
+ cDelta = 10;
 var
  l_PrevShape : ImsShape;
  l_Shape : ImsShape;
