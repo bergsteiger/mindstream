7491b441ddda404155325f223ec5dda36434c003
Alex W. Lulin <lulinalex@gmail.com>
2015-02-19 01:01:06 +0300

    - добавляем вспомогательную структуру данных TmsPointF.

--- Core/msLineF.pas
+++ Core/msLineF.pas
@@ -9,12 +9,33 @@ uses
  ;
 
 type
+ Pixel = Single;
+
+ TmsPointF = record
+ public
+  P : TPointF;
+  constructor Create(const aPoint: TPointF); overload;
+  constructor Create(aX: Pixel; aY: Pixel); overload;
+ end;//TmsPointF
+
  TmsLineF = record
  public
   procedure ToLog(aLog: TmsLog);
  end;//TmsLineF
 
 implementation
+
+// TmsPointF
+
+constructor TmsPointF.Create(const aPoint: TPointF);
+begin
+ P := aPoint;
+end;
+
+constructor TmsPointF.Create(aX: Pixel; aY: Pixel);
+begin
+ Create(TPointF.Create(aX, aY));
+end;
 
 // TmsLineF
 
