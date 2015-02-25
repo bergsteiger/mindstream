ca694caad47ae47a7e1f6853f87ece58d904cee4
Alex W. Lulin <lulinalex@gmail.com>
2015-02-25 21:39:30 +0300

    - делаем стрелку реализаци пунктирной.

--- ConcreteShapes/ForUML/msGeneralization.pas
+++ ConcreteShapes/ForUML/msGeneralization.pas
@@ -3,13 +3,26 @@ unit msGeneralization;
 interface
 
 uses
+ msInterfaces,
  msConnector
  ;
 
 type
  TmsGeneralization = class(TmsConnector)
+ protected
+  procedure TransformDrawOptionsContext(var theCtx: TmsDrawOptionsContext); override;
  end;//TmsGeneralization
 
 implementation
+
+uses
+ FMX.Graphics
+ ;
+
+procedure TmsGeneralization.TransformDrawOptionsContext(var theCtx: TmsDrawOptionsContext);
+begin
+ inherited;
+ //theCtx.rStrokeDash := TStrokeDash.Dash;
+end;
 
 end.
--- ConcreteShapes/ForUML/msRealization.pas
+++ ConcreteShapes/ForUML/msRealization.pas
@@ -3,13 +3,27 @@ unit msRealization;
 interface
 
 uses
+ msInterfaces,
  msConnector
  ;
 
 type
  TmsRealization = class(TmsConnector)
+ protected
+  procedure TransformDrawOptionsContext(var theCtx: TmsDrawOptionsContext); override;
  end;//TmsRealization
 
 implementation
+
+uses
+ FMX.Graphics
+ ;
+
+procedure TmsRealization.TransformDrawOptionsContext(var theCtx: TmsDrawOptionsContext);
+begin
+ inherited;
+ theCtx.rStrokeDash := TStrokeDash.Dash;
+ theCtx.rStrokeThickness := 2;
+end;
 
 end.
