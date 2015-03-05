61f7629b65acc1bf23b11860c3c2f599b71c1e64
Alex W. Lulin <lulinalex@gmail.com>
2015-03-05 03:06:40 +0300

    - переносим TransformDrawOptionsContext в предка.

--- MetaShapes/msShapeClass.pas
+++ MetaShapes/msShapeClass.pas
@@ -30,7 +30,6 @@ type
   function IsOurInstance(const aShape: ImsShape): Boolean;
   function NullClick(const aHolder: ImsDiagrammsHolder): Boolean;
   function Stereotype: String;
-  procedure TransformDrawOptionsContext(var theCtx: TmsDrawOptionsContext);
   function ParentMC: ImsShapeClass; override;
   function SetFillColor(aColor: TAlphaColor): ImsTunableShapeClass;
   function SetStrokeThickness(aValue: Pixel): ImsTunableShapeClass;
@@ -104,17 +103,6 @@ begin
  Assert(f_ShapeClass <> nil);
  Result := f_ShapeClass.ClassName;
  Result := Copy(Result, 4, Length(Result) - 3);
-end;
-
-procedure TmsShapeClass.TransformDrawOptionsContext(var theCtx: TmsDrawOptionsContext);
-begin
- Assert(f_ShapeClass <> nil);
- if (ParentMC <> nil) then
-  ParentMC.TransformDrawOptionsContext(theCtx);
- if f_FillColor.rIsSet then
-  theCtx.rFillColor := f_FillColor.rValue;
- if f_StrokeThickness.rIsSet then
-  theCtx.rStrokeThickness := f_StrokeThickness.rValue;
 end;
 
 function TmsShapeClass.ParentMC: ImsShapeClass;
--- MetaShapes/msShapeClassPrim.pas
+++ MetaShapes/msShapeClassPrim.pas
@@ -15,6 +15,7 @@ type
   f_StrokeThickness : TmsPixelRec;
  protected
   function ParentMC: ImsShapeClass; virtual;
+  procedure TransformDrawOptionsContext(var theCtx: TmsDrawOptionsContext);
  end;//TmsShapeClassPrim
 
 implementation
@@ -25,6 +26,16 @@ function TmsShapeClassPrim.ParentMC: ImsShapeClass;
 begin
  Result := nil;
  Assert(false, 'Не реализовано');
+end;
+
+procedure TmsShapeClassPrim.TransformDrawOptionsContext(var theCtx: TmsDrawOptionsContext);
+begin
+ if (ParentMC <> nil) then
+  ParentMC.TransformDrawOptionsContext(theCtx);
+ if f_FillColor.rIsSet then
+  theCtx.rFillColor := f_FillColor.rValue;
+ if f_StrokeThickness.rIsSet then
+  theCtx.rStrokeThickness := f_StrokeThickness.rValue;
 end;
 
 end.
