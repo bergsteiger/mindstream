8a525663e6050834d218734de0696d8eb2399928
Alex W. Lulin <lulinalex@gmail.com>
2015-03-13 02:30:15 +0300

    - переносим InitialWidth на <<стереотип>>.

--- ConcreteShapes/msBlackRectangle.pas
+++ ConcreteShapes/msBlackRectangle.pas
@@ -10,17 +10,8 @@ uses
 
 type
  TmsBlackRectangle = class(TmsRectangle)
- protected
-  class function GetInitialWidth: Single; override;
  end;//TmsBlackRectangle
 
 implementation
-
-// TmsBlackRectangle
-
-class function TmsBlackRectangle.GetInitialWidth: Single;
-begin
- Result := 15;
-end;
 
 end.
