0f7ffa9c04ceaa16f1c9aa2661613796b313b89b
Alex W. Lulin <lulinalex@gmail.com>
2015-03-24 01:08:29 +0300

    - переносим TmsPointCircle на мета-модель.

--- Tests/Module/TestSaveMoverToPNG.pas
+++ Tests/Module/TestSaveMoverToPNG.pas
@@ -49,7 +49,7 @@ uses
   TypInfo,
 
   msConnector,
-  msPointCircle
+  msShape
   ;
 
 // TTestSaveMoverToPNG
@@ -101,7 +101,7 @@ begin
   CheckFalse(aShape.DrawBounds.Contains(l_ClickPoint), 'Фигура не должна содержать точку клика');
   aMover.EndTo(l_Ctx);
   CheckFalse(aShape.DrawBounds.Contains(l_ClickPoint), 'Видимо не попали в кнопку и фигура сместилась в точку клика');
-  l_S := TmsPointCircle.Create(l_ClickPoint);
+  l_S := TmsShape.NamedMC('PointCircle').CreateShape(l_ClickPoint);
   Assert(l_S.StartPoint = l_ClickPoint);
   aDiagramm.AddShape(l_S);
  end;//aMover <> nil
