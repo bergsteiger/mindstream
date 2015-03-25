9110aab802a88d3537e3c169d11249d0fc22d47c
Alex W. Lulin <lulinalex@gmail.com>
2015-03-25 22:03:03 +0300

    - делаем TmsRemoveIcon фабрикой, а не настоящим примитивом.

--- SpecialShapes/msRemoveIcon.pas
+++ SpecialShapes/msRemoveIcon.pas
@@ -3,15 +3,33 @@
 interface
 
 uses
- msSVGShape,
  msInterfaces
  ;
 
 type
- TmsRemoveIcon = class(TmsSVGShape)
+ TmsRemoveIcon = class
  // - "Иконка для удаления фигур"
+ public
+  class function Create(const aPoint: TPointF): ImsShape; overload;
+  class function Create: ImsShape; overload;
  end;//TmsRemoveIcon
 
 implementation
+
+uses
+ msShape
+ ;
+
+// TmsRemoveIcon
+
+class function TmsRemoveIcon.Create(const aPoint: TPointF): ImsShape;
+begin
+ Result := TmsShape.NamedMC('RemoveIcon').CreateShape(aPoint);
+end;
+
+class function TmsRemoveIcon.Create: ImsShape;
+begin
+ Result := Create(TPointF.Create(0, 0));
+end;
 
 end.
