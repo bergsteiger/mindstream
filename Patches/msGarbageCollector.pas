0a74edf07ba2a94764886be5a54de9cbaf87727f
Alex W. Lulin <lulinalex@gmail.com>
2015-04-02 00:19:09 +0300

    - Расширяем функционал.

--- Patches/msGarbageCollector.pas
+++ Patches/msGarbageCollector.pas
@@ -7,7 +7,7 @@ uses
  ;
 
 type
- TmsGarbageCollector = class
+ TmsGarbageCollector = class(TList<IUnknown>)
   //  ласс позвол€ющий боротьс€ с http://programmingmindstream.blogspot.com/2015/04/json-unmarshal-tinterfacedobject.html
   // ѕутЄм отложенного удалени€ объектов, которым был взведЄн лишний AddRef.
  end;//TmsGarbageCollector
