66c7cd600ff73a841b769886225186cdd99f9364
Alex W. Lulin <lulinalex@gmail.com>
2015-04-03 02:19:09 +0300

    - чистим код.

--- Tests/Module/TestSaveMoverToPNG.pas
+++ Tests/Module/TestSaveMoverToPNG.pas
@@ -143,7 +143,7 @@ end;
 procedure TmsConnectorDrawTest.ModifyDiagramm(const aDiagramm: ImsDiagramm);
 begin
  inherited;
- if not f_Context.ShapeClass.IsLineLike then
+ if not Self.ShapeClass.IsLineLike then
   AddConnectorsToDiagramm(aDiagramm);
 end;
 
