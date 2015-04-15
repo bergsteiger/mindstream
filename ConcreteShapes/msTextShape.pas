964ce9c35c825dccdbf296b72979e7e430e3d40e
Alex W. Lulin <lulinalex@gmail.com>
2015-04-16 01:51:19 +0300

    - И ещё шажок к анемичной модели. Разделяем данные и код.

--- ConcreteShapes/msTextShape.pas
+++ ConcreteShapes/msTextShape.pas
@@ -3,46 +3,14 @@ unit msTextShape;
 interface
 
 uses
- System.Types,
- FMX.Graphics,
- FMX.Types,
- System.UITypes,
- msInterfaces,
  msRectangularShape
  ;
 
 type
  TmsTextShape = class(TmsRectangularShape)
- protected
-  procedure DoDrawTo(const aCtx: TmsDrawContext); override;
  end;//TmsTextShape
 
 implementation
-
-// TmsTextShape
-
-procedure TmsTextShape.DoDrawTo(const aCtx: TmsDrawContext);
-var
- l_msPointContext: TRectF;
-begin
- l_msPointContext := DrawBounds;
- aCtx.rCanvas.FillText(l_msPointContext,
-                       Self.ShapeClass.Text,
-                       false,
-                       1,
-                       [],
-                       TTextAlign.Center,
-                       TTextAlign.Center);
- if aCtx.rMoving then
- begin
-  aCtx.rCanvas.DrawRect(l_msPointContext,
-                   0{CornerRadius},
-                   0{CornerRadius},
-                   AllCorners,
-                   1,
-                   TCornerType.Round);
- end;//aCtx.rMoving
-end;
 
 end.
 
