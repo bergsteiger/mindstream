ecdcc8df230248b72ef4f743ec2b1855a4ad8c8a
Alex W. Lulin <lulinalex@gmail.com>
2015-04-17 01:47:29 +0300

    - заготовочка.

---
+++ Views/msLineView.pas
@@ -0,0 +1,45 @@
+unit msLineView;
+
+interface
+
+uses
+ msInterfaces,
+ msShapeView
+ ;
+
+type
+ TmsLineView = class(TmsShapeView)
+  protected
+   procedure DrawShape(const aCtx: TmsDrawContext; const aShape: ImsShape); override;
+ end;//TmsLineView
+
+implementation
+
+uses
+ msShape
+ ;
+
+// TmsLineView
+
+procedure TmsLineView.DrawShape(const aCtx: TmsDrawContext; const aShape: ImsShape);
+var
+ l_Proxy : ImsShape;
+ l_FinishPoint: TPointF;
+begin
+ if (aShape.StartPoint = aShape.FinishPoint) then
+ begin
+  l_Proxy := TmsShape.NamedMC('PointCircle').CreateShape(aShape.StartPoint);
+  try
+   l_Proxy.DrawTo(aCtx);
+  finally
+   l_Proxy := nil;
+  end;//try..finally
+ end//StartPoint = FinishPoint
+ else
+ begin
+  l_FinishPoint := aShape.GetFinishPointForDraw;
+  aCtx.rCanvas.DrawLine(aShape.StartPoint, l_FinishPoint, 1);
+ end;//StartPoint = FinishPoint
+end;
+
+end.
