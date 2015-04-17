c8e9039a67505ee6b5cc98f4e033796a80044f10
Alex W. Lulin <lulinalex@gmail.com>
2015-04-18 00:38:49 +0300

    - обобщаем код.

--- ConcreteShapes/msLineWithArrow.pas
+++ ConcreteShapes/msLineWithArrow.pas
@@ -15,7 +15,6 @@ type
  TmsLineWithArrow = class(TmsLine)
  protected
   function GetFinishPointForDraw: TPointF; override;
-  function GetDrawBounds: TRectF; override;
  end;//TmsLineWithArrow
 
 implementation
@@ -28,21 +27,6 @@ uses
  ;
 
 // TmsLineWithArrow
-
-function TmsLineWithArrow.GetDrawBounds: TRectF;
-begin
- Result := inherited GetDrawBounds;
- if SameValue(Result.Left, Result.Right) then
- begin
-  Result.Right := Result.Left + Self.ShapeClass.ArrowHeadShapeMC.InitialHeight;
-  Result.Left := Result.Left - Self.ShapeClass.ArrowHeadShapeMC.InitialHeight;
- end;//SameValue(Result.Left, Result.Right)
- if SameValue(Result.Top, Result.Bottom) then
- begin
-  Result.Bottom := Result.Top + Self.ShapeClass.ArrowHeadShapeMC.InitialHeight;
-  Result.Top := Result.Top - Self.ShapeClass.ArrowHeadShapeMC.InitialHeight;
- end;//SameValue(Result.Top, Result.Bottom)
-end;
 
 function TmsLineWithArrow.GetFinishPointForDraw: TPointF;
 var
