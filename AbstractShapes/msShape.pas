459b0b809d5fecc6ad89c0fe13cb268fc7e9e065
Alex W. Lulin <lulinalex@gmail.com>
2015-04-29 01:12:31 +0300

    - чистим код.

--- AbstractShapes/msShape.pas
+++ AbstractShapes/msShape.pas
@@ -222,64 +222,10 @@ end;
 procedure TmsShape.DoDrawTo(const aCtx: TmsDrawContext);
 var
  l_V : ImsShapeView;
-var
- l_Proxy : ImsShape;
- l_OriginalMatrix: TMatrix;
- l_Matrix: TMatrix;
- l_Angle : Single;
- l_CenterPoint,
- l_LineFinishPoint : TPointF;
 begin
  l_V := ShapeClass.ShapeView;
  if (l_V <> nil) then
   l_V.DrawShape(aCtx, Self);
- if (Self.ShapeClass.ArrowHeadShapeMC <> nil) then
- begin
-  if (Self.StartPoint <> Self.FinishPoint) then
-  begin
-   l_OriginalMatrix := aCtx.rCanvas.Matrix;
-   try
-    l_LineFinishPoint := TPointF.Create(Self.FinishPoint.X - Self.ShapeClass.ArrowHeadShapeMC.InitialHeight / 2,
-                                        Self.FinishPoint.Y);
-    l_Proxy := Self.ShapeClass.ArrowHeadShapeMC.CreateShape(l_LineFinishPoint);
-    try
-     // in Radian
-     l_Angle := TmsShape.AngleBetween(Self.StartPoint, Self.FinishPoint);
-
-     l_CenterPoint := Self.FinishPoint;
-
-     l_Matrix := TMatrix.Identity;
-     // - СНИМАЕМ оригинальную матрицу, точнее берём ЕДИНИЧНУЮ матрицу
-     // https://ru.wikipedia.org/wiki/%D0%95%D0%B4%D0%B8%D0%BD%D0%B8%D1%87%D0%BD%D0%B0%D1%8F_%D0%BC%D0%B0%D1%82%D1%80%D0%B8%D1%86%D0%B0
-     l_Matrix := l_Matrix * TMatrix.CreateTranslation(-l_CenterPoint.X, -l_CenterPoint.Y);
-
-     // - задаём точку, вокруг которой вертим
-     l_Matrix := l_Matrix * TMatrix.CreateRotation(l_Angle);
-
-     // - задаём угол поворота
-     l_Matrix := l_Matrix * TMatrix.CreateTranslation(l_CenterPoint.X, l_CenterPoint.Y);
-
-     // - задаём начало координат
-     l_Matrix := l_Matrix * l_OriginalMatrix;
-
-     // - ПРИМЕНЯЕМ оригинальную матрицу
-     // Иначе например ОРИГИНАЛЬНЫЙ параллельный перенос - не будет работать.
-     // https://ru.wikipedia.org/wiki/%D0%9F%D0%B0%D1%80%D0%B0%D0%BB%D0%BB%D0%B5%D0%BB%D1%8C%D0%BD%D1%8B%D0%B9_%D0%BF%D0%B5%D1%80%D0%B5%D0%BD%D0%BE%D1%81
-
-     aCtx.rCanvas.SetMatrix(l_Matrix);
-     // - применяем нашу "комплексную" матрицу
-
-     l_Proxy.DrawTo(aCtx);
-     // - отрисовываем примитив с учётом матрицы преобразований
-    finally
-     l_Proxy := nil;
-    end;//try..finally
-   finally
-     aCtx.rCanvas.SetMatrix(l_OriginalMatrix);
-   // - восстанавливаем ОРИГИНАЛЬНУЮ матрицу
-   end;//try..finally
-  end;
- end;//(StartPoint <> FinishPoint)
 end;
 
 function TmsShape.IsNeedsSecondClick : Boolean;
@@ -470,6 +416,13 @@ var
  l_R: TRectF;
  l_AL : TmsAdditionalLineCoeff;
  l_C : Single;
+var
+ l_Proxy : ImsShape;
+ l_OriginalMatrix: TMatrix;
+ l_Matrix: TMatrix;
+ l_Angle : Single;
+ l_CenterPoint,
+ l_LineFinishPoint : TPointF;
 begin
  l_Ctx := DrawOptionsContext(aCtx);
  aCtx.rCanvas.Fill.Color := l_Ctx.rFillColor;
@@ -477,14 +430,63 @@ begin
  aCtx.rCanvas.Stroke.Color := l_Ctx.rStrokeColor;
  aCtx.rCanvas.Stroke.Thickness := l_Ctx.rStrokeThickness;
 
-  // for Androide Lines
+  // for Android Lines
  aCtx.rCanvas.Stroke.Kind := TBrushKind.Solid;
  aCtx.rCanvas.Fill.Kind := TBrushKind.Solid;
 
  l_DrawContext := aCtx;
  l_DrawContext.rOpacity := l_Ctx.rOpacity;
  l_DrawContext.rLineOpacity := l_Ctx.rLineOpacity;
+
  DoDrawTo(l_DrawContext);
+
+ if (Self.ShapeClass.ArrowHeadShapeMC <> nil) then
+ begin
+  if (Self.StartPoint <> Self.FinishPoint) then
+  begin
+   l_OriginalMatrix := aCtx.rCanvas.Matrix;
+   try
+    l_LineFinishPoint := TPointF.Create(Self.FinishPoint.X - Self.ShapeClass.ArrowHeadShapeMC.InitialHeight / 2,
+                                        Self.FinishPoint.Y);
+    l_Proxy := Self.ShapeClass.ArrowHeadShapeMC.CreateShape(l_LineFinishPoint);
+    try
+     // in Radian
+     l_Angle := TmsShape.AngleBetween(Self.StartPoint, Self.FinishPoint);
+
+     l_CenterPoint := Self.FinishPoint;
+
+     l_Matrix := TMatrix.Identity;
+     // - СНИМАЕМ оригинальную матрицу, точнее берём ЕДИНИЧНУЮ матрицу
+     // https://ru.wikipedia.org/wiki/%D0%95%D0%B4%D0%B8%D0%BD%D0%B8%D1%87%D0%BD%D0%B0%D1%8F_%D0%BC%D0%B0%D1%82%D1%80%D0%B8%D1%86%D0%B0
+     l_Matrix := l_Matrix * TMatrix.CreateTranslation(-l_CenterPoint.X, -l_CenterPoint.Y);
+
+     // - задаём точку, вокруг которой вертим
+     l_Matrix := l_Matrix * TMatrix.CreateRotation(l_Angle);
+
+     // - задаём угол поворота
+     l_Matrix := l_Matrix * TMatrix.CreateTranslation(l_CenterPoint.X, l_CenterPoint.Y);
+
+     // - задаём начало координат
+     l_Matrix := l_Matrix * l_OriginalMatrix;
+
+     // - ПРИМЕНЯЕМ оригинальную матрицу
+     // Иначе например ОРИГИНАЛЬНЫЙ параллельный перенос - не будет работать.
+     // https://ru.wikipedia.org/wiki/%D0%9F%D0%B0%D1%80%D0%B0%D0%BB%D0%BB%D0%B5%D0%BB%D1%8C%D0%BD%D1%8B%D0%B9_%D0%BF%D0%B5%D1%80%D0%B5%D0%BD%D0%BE%D1%81
+
+     aCtx.rCanvas.SetMatrix(l_Matrix);
+     // - применяем нашу "комплексную" матрицу
+
+     l_Proxy.DrawTo(aCtx);
+     // - отрисовываем примитив с учётом матрицы преобразований
+    finally
+     l_Proxy := nil;
+    end;//try..finally
+   finally
+     aCtx.rCanvas.SetMatrix(l_OriginalMatrix);
+   // - восстанавливаем ОРИГИНАЛЬНУЮ матрицу
+   end;//try..finally
+  end;//Self.StartPoint <> Self.FinishPoint
+ end;//Self.ShapeClass.ArrowHeadShapeMC <> nil
 
  l_AL := ShapeClass.AdditionalLinesH;
  if (Length(l_AL) > 0) then
