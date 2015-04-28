8e20fe418a10935b8c8ba8963eeecfb7f25a545d
Alex W. Lulin <lulinalex@gmail.com>
2015-04-29 01:03:20 +0300

    - чистим код.

--- AbstractShapes/msPolylineShape.pas
+++ AbstractShapes/msPolylineShape.pas
@@ -32,9 +32,12 @@ begin
  l_Path := TPathData.Create;
  try
   for I := 0 to High(l_P) do
-   if I = 0
-    then l_Path.MoveTo(l_P[I])
-    else l_Path.LineTo(l_P[I]);
+  begin
+   if (I = 0) then
+    l_Path.MoveTo(l_P[I])
+   else
+    l_Path.LineTo(l_P[I]);
+  end;//for I
   aCtx.rCanvas.DrawPath(l_Path, 1);
  finally
   FreeAndNil(l_Path);
