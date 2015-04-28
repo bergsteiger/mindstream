dd9cd60afd8419830083f02a7ba8d50fe131912c
Alex W. Lulin <lulinalex@gmail.com>
2015-04-29 01:44:39 +0300

    - чистим код.

--- AbstractShapes/msRectangularShape.pas
+++ AbstractShapes/msRectangularShape.pas
@@ -13,30 +13,17 @@ uses
 
 type
  TmsRectangularShape = class abstract(TmsPointedShape)
- protected
-  function GetDrawBounds: TRectF; override;
  public
   function ContainsPt(const aPoint: TPointF): Boolean; override;
- end; // TmsRectangularShape
+ end;//TmsRectangularShape
 
 implementation
 
-{ TmsRectangularShape }
+// TmsRectangularShape
 
 function TmsRectangularShape.ContainsPt(const aPoint: TPointF): Boolean;
 begin
  Result := DrawBounds.Contains(aPoint);
-end;
-
-function TmsRectangularShape.GetDrawBounds: TRectF;
-var
- l_StartPoint, l_FinalPoint : TPointF;
-begin
- l_StartPoint := TPointF.Create(StartPoint.X - ShapeClass.InitialWidth / 2,
-                               StartPoint.Y - ShapeClass.InitialHeight / 2);
- l_FinalPoint := TPointF.Create(StartPoint.X + ShapeClass.InitialWidth / 2,
-                            StartPoint.Y + ShapeClass.InitialHeight / 2);
- Result := TRectF.Create(l_StartPoint, l_FinalPoint);
 end;
 
 end.
