495eaa4e43b79c68f667baa3d3c892acfa6cab3d
Alex W. Lulin <lulinalex@gmail.com>
2015-04-29 01:56:58 +0300

    - чистим код.

--- AbstractShapes/msRectangularShape.pas
+++ AbstractShapes/msRectangularShape.pas
@@ -3,27 +3,13 @@ unit msRectangularShape;
 interface
 
 uses
- System.Types,
- FMX.Graphics,
- FMX.Types,
- System.UITypes,
- msInterfaces,
  msPointedShape
  ;
 
 type
  TmsRectangularShape = class abstract(TmsPointedShape)
- public
-  function ContainsPt(const aPoint: TPointF): Boolean; override;
  end;//TmsRectangularShape
 
 implementation
-
-// TmsRectangularShape
-
-function TmsRectangularShape.ContainsPt(const aPoint: TPointF): Boolean;
-begin
- Result := DrawBounds.Contains(aPoint);
-end;
 
 end.
