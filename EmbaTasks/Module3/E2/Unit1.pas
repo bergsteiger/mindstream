b9b53bf21e6d7b77226b2049a7db11482abe9255
Alex W. Lulin <lulinalex@gmail.com>
2015-06-02 11:28:00 +0300

    - заготовочка.

---
+++ EmbaTasks/Module3/E2/Unit1.pas
@@ -0,0 +1,35 @@
+unit Unit1;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls;
+
+type
+  TForm1 = class(TForm)
+    Label1: TLabel;
+    F: TButton;
+    Edit1: TEdit;
+    C: TButton;
+    procedure FClick(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+procedure TForm1.FClick(Sender: TObject);
+begin
+ Label1.Caption := Edit1.Text;
+ Edit1.Text := '';
+end;
+
+end.
