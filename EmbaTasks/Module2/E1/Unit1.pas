cfd171621b6e31c3e4897b8997e1433742fe129e
Alex W. Lulin <lulinalex@gmail.com>
2015-06-02 10:46:22 +0300

    - Пример 1.

---
+++ EmbaTasks/Module2/E1/Unit1.pas
@@ -0,0 +1,39 @@
+unit Unit1;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls;
+
+type
+  TForm1 = class(TForm)
+    Label1: TLabel;
+    Button1: TButton;
+    Button2: TButton;
+    procedure Button1Click(Sender: TObject);
+    procedure Button2Click(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+ Label1.Enabled := true;
+end;
+
+procedure TForm1.Button2Click(Sender: TObject);
+begin
+ Label1.Enabled := false;
+end;
+
+end.
