b3a2c8573b4717afd2c3743d6cabe88fc173c00c
Alex W. Lulin <lulinalex@gmail.com>
2015-06-04 02:22:19 +0300

    - заготовочка.

--- EmbaTasks/Module5/E3/Unit1.pas
+++ EmbaTasks/Module5/E3/Unit1.pas
@@ -8,15 +8,6 @@ uses
 
 type
   TForm1 = class(TForm)
-    btRed: TButton;
-    lbRed: TLabel;
-    lbYellow: TLabel;
-    lbGreen: TLabel;
-    btYellow: TButton;
-    btGreen: TButton;
-    procedure btRedClick(Sender: TObject);
-    procedure btYellowClick(Sender: TObject);
-    procedure btGreenClick(Sender: TObject);
   private
     { Private declarations }
   public
@@ -29,26 +20,5 @@ var
 implementation
 
 {$R *.dfm}
-
-procedure TForm1.btGreenClick(Sender: TObject);
-begin
- lbRed.Visible := false;
- lbYellow.Visible := false;
- lbGreen.Visible := true;
-end;
-
-procedure TForm1.btRedClick(Sender: TObject);
-begin
- lbRed.Visible := true;
- lbYellow.Visible := false;
- lbGreen.Visible := false;
-end;
-
-procedure TForm1.btYellowClick(Sender: TObject);
-begin
- lbRed.Visible := false;
- lbYellow.Visible := true;
- lbGreen.Visible := false;
-end;
 
 end.
