a69113e2ddbca4e128775b96413634a0d42ff8c4
Alex W. Lulin <lulinalex@gmail.com>
2015-06-09 00:20:48 +0300

    - решаем задачу.

--- EmbaTasks/Module7/E2/Unit1.pas
+++ EmbaTasks/Module7/E2/Unit1.pas
@@ -8,6 +8,10 @@ uses
 
 type
   TForm1 = class(TForm)
+    Edit1: TEdit;
+    Button1: TButton;
+    Label1: TLabel;
+    procedure Button1Click(Sender: TObject);
   private
     { Private declarations }
   public
@@ -20,5 +24,30 @@ var
 implementation
 
 {$R *.dfm}
+
+procedure TForm1.Button1Click(Sender: TObject);
+var
+ l_Y : Integer;
+ l_IsLeap : Boolean;
+begin
+ l_Y := StrToInt(Edit1.Text);
+ l_IsLeap := false;
+
+ if (l_Y mod 4 = 0) then
+ begin
+  if (l_Y mod 100 = 0) then
+  begin
+   if (l_Y mod 400 = 0) then
+    l_IsLeap := true;
+  end//l_Y mod 100 = 0
+  else
+   l_IsLeap := true;
+ end;//l_Y mod 4
+
+ if l_IsLeap then
+  Label1.Caption := 'true'
+ else
+  Label1.Caption := 'false';
+end;
 
 end.
