f709cf9b6ad088e917c44bea4051079de1c1bd2a
Alex W. Lulin <lulinalex@gmail.com>
2015-06-09 01:25:59 +0300

    - решаем задачу.

--- EmbaTasks/Module8/E2/Unit1.pas
+++ EmbaTasks/Module8/E2/Unit1.pas
@@ -34,6 +34,21 @@ implementation
 {$R *.dfm}
 
 procedure TForm1.Button1Click(Sender: TObject);
+
+ procedure P(A, B, C: Extended; var theP : Extended);
+ begin
+  theP := A + B + C;
+ end;
+
+ procedure S(A, B, C: Extended; var theS : Extended);
+ var
+  l_P : Extended;
+ begin
+  P(A, B, C, l_P);
+  l_P := l_P / 2;
+  theS := Sqrt(l_P * (l_P - A) * (l_P - B) * (l_P - C));
+ end;
+
 var
  l_A1 : Extended;
  l_B1 : Extended;
@@ -54,6 +69,12 @@ begin
  l_A2 := StrToInt(A2.Text);
  l_B2 := StrToInt(B2.Text);
  l_C2 := StrToInt(C2.Text);
+
+ P(l_A1, l_B1, l_C1, l_P1);
+ P(l_A2, l_B2, l_C2, l_P2);
+
+ S(l_A1, l_B1, l_C1, l_S1);
+ S(l_A2, l_B2, l_C2, l_S2);
 
  P1.Caption := FloatToStr(l_P1);
  P2.Caption := FloatToStr(l_P2);
