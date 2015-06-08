eb42bca1b35d7d75a509686515f0de290919a97d
Alex W. Lulin <lulinalex@gmail.com>
2015-06-08 23:44:10 +0300

    - решаем задачу.

--- EmbaTasks/Module6/E4/Unit1.pas
+++ EmbaTasks/Module6/E4/Unit1.pas
@@ -12,6 +12,7 @@ type
     n: TEdit;
     Button1: TButton;
     Label1: TLabel;
+    procedure Button1Click(Sender: TObject);
   private
     { Private declarations }
   public
@@ -24,5 +25,23 @@ var
 implementation
 
 {$R *.dfm}
+
+procedure TForm1.Button1Click(Sender: TObject);
+var
+ l_a : Integer;
+ l_n : Integer;
+ l_Summ : Integer;
+ l_I : Integer;
+begin
+ l_a := StrToInt(a.Text);
+ l_n := StrToInt(n.Text);
+ l_Summ := 0;
+ for l_I := l_a to l_a + l_n - 1 do
+  l_Summ := l_Summ + l_I;
+ if (l_Summ mod 2 = 0) then
+  Label1.Caption := 'true'
+ else
+  Label1.Caption := 'false';
+end;
 
 end.
