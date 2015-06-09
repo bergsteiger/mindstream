a22ddabcf338659b4dfdc878c8b82b1946cb4bda
Alex W. Lulin <lulinalex@gmail.com>
2015-06-09 23:37:24 +0300

    - Заготовочка.

--- EmbaTasks/Module8/E4/Unit1.pas
+++ EmbaTasks/Module8/E4/Unit1.pas
@@ -38,8 +38,26 @@ type
  end;//TP
 
 procedure TForm1.Button1Click(Sender: TObject);
+
+ procedure S(const v1, v2, v3: TP; var theS: Extended);
+ var
+  a, b, c : Extended;
+  P : Extended;
+  p2 : Extended;
+ begin
+  a := Sqrt(Sqr(v2.X - v3.X) + Sqr(v2.Y - v3.Y));
+  b := Sqrt(Sqr(v2.X - v1.X) + Sqr(v2.Y - v1.Y));
+  c := Sqrt(Sqr(v1.X - v3.X) + Sqr(v1.Y - v3.Y));
+  P := a + b + c;
+
+  p2 := P / 2;
+
+  theS := Sqrt(p2*(p2-a)*(p2-b)*(p2-c));
+ end;
+
 var
  v1, v2, v3, v4 : TP;
+ S1, S2 : Extended;
 begin
  v1.X := StrToFloat(v1x.Text);
  v1.Y := StrToFloat(v1y.Text);
@@ -51,7 +69,10 @@ begin
  v3.Y := StrToFloat(v3y.Text);
 
  v4.X := StrToFloat(v4x.Text);
+
  v4.Y := StrToFloat(v4y.Text);
+ S(v4, v1, v2, S1);
+ S(v4, v3, v2, S2);
 end;
 
 end.
