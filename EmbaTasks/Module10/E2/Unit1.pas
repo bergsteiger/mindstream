47d378409e4859cff4a1878e4feb34016be438f1
Ingword <instigator21@gmail.com>
2015-07-19 15:26:05 +0300

    Заготовка под дом

---
+++ EmbaTasks/Module10/E2/Unit1.pas
@@ -0,0 +1,50 @@
+unit Unit1;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Vcl.ExtCtrls;
+
+type
+  TForm1 = class(TForm)
+    pbxEx: TPaintBox;
+    pnlButtons: TPanel;
+    btnSnowMan: TButton;
+    procedure btnSnowManClick(Sender: TObject);
+  private
+    { Private declarations }
+    procedure DrawSnowMan;
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+procedure TForm1.btnSnowManClick(Sender: TObject);
+begin
+  DrawSnowMan;
+end;
+
+procedure TForm1.DrawSnowMan;
+var
+  Center : TPoint;
+begin
+  Center.X := pbxEx.Width div 2;
+  Center.Y := pbxEx.Height div 2;
+
+  pbxEx.Canvas.Pen.Color:= clBlack;
+  pbxEx.Canvas.Pen.Width:= 2;
+
+  pbxEx.Canvas.Brush.Color := clWhite;
+  pbxEx.Canvas.Pixels[Center.X, Center.Y] := clBlack;
+
+  //pbxEx.Canvas.LineTo(Center.X, Center.Y);
+end;
+
+end.
