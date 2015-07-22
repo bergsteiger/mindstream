13b1630365d36f00e0a1bbf607adf0ca35811753
Ingword <instigator21@gmail.com>
2015-07-23 00:54:47 +0300

    Сделали 4 таск

---
+++ EmbaTasks/Module11/E4/Unit1.dfm
@@ -0,0 +1,73 @@
+object frmMain: TfrmMain
+  Left = 0
+  Top = 0
+  Caption = 'Main'
+  ClientHeight = 452
+  ClientWidth = 600
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'Tahoma'
+  Font.Style = []
+  OldCreateOrder = False
+  PixelsPerInch = 96
+  TextHeight = 13
+  object pbxEx: TPaintBox
+    Left = 0
+    Top = 52
+    Width = 600
+    Height = 400
+    Align = alClient
+    ExplicitTop = 0
+    ExplicitWidth = 445
+    ExplicitHeight = 273
+  end
+  object pnlButtons: TPanel
+    Left = 0
+    Top = 0
+    Width = 600
+    Height = 52
+    Align = alTop
+    TabOrder = 0
+    object lblColumnCount: TLabel
+      Left = 162
+      Top = 19
+      Width = 65
+      Height = 13
+      Caption = 'Column count'
+    end
+    object lblRowcount: TLabel
+      Left = 24
+      Top = 19
+      Width = 51
+      Height = 13
+      Caption = 'Row count'
+    end
+    object btnDraw: TButton
+      Left = 416
+      Top = 14
+      Width = 129
+      Height = 25
+      Caption = 'Draw'
+      TabOrder = 0
+      OnClick = btnDrawClick
+    end
+    object edtRowCount: TEdit
+      Left = 93
+      Top = 16
+      Width = 41
+      Height = 21
+      TabOrder = 1
+      Text = '10'
+    end
+    object edtColumnCount: TEdit
+      Left = 231
+      Top = 16
+      Width = 42
+      Height = 21
+      TabOrder = 2
+      Text = '10'
+    end
+  end
+end
---
+++ EmbaTasks/Module11/E4/Unit1.pas
@@ -0,0 +1,94 @@
+unit Unit1;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Vcl.ExtCtrls;
+
+type
+  TfrmMain = class(TForm)
+    pbxEx: TPaintBox;
+    pnlButtons: TPanel;
+    btnDraw: TButton;
+    edtRowCount: TEdit;
+    edtColumnCount: TEdit;
+    lblColumnCount: TLabel;
+    lblRowcount: TLabel;
+    procedure btnDrawClick(Sender: TObject);
+  private
+    { Private declarations }
+    procedure Draw;
+  public
+    { Public declarations }
+  end;
+
+var
+  frmMain: TfrmMain;
+
+implementation
+
+{$R *.dfm}
+
+procedure TfrmMain.btnDrawClick(Sender: TObject);
+begin
+  Draw;
+end;
+
+procedure TfrmMain.Draw;
+var
+  PaintCenter,
+  SquareTopLeft,
+  SquareRightBottom : TPoint;
+
+  RowCount,
+  ColumnCount,
+  RowStep,
+  ColumnStep,
+  Index,
+  RowIndex,
+  ColumnIndex : integer;
+begin
+  PaintCenter.X := pbxEx.Width div 2;
+  PaintCenter.Y := pbxEx.Height div 2;
+
+  pbxEx.Canvas.Pen.Color:= clBlack;
+  pbxEx.Canvas.Pen.Width:= 2;
+
+  RowCount := StrToInt(edtRowCount.Text);
+  ColumnCount := StrToInt(edtColumnCount.Text);
+
+  RowStep := pbxEx.Height div RowCount;
+  ColumnStep := pbxEx.Width div ColumnCount;
+
+  for Index := 1 to RowCount do
+  begin
+    pbxEx.Canvas.MoveTo(0, RowStep * Index);
+    pbxEx.Canvas.LineTo(pbxEx.Width, RowStep * Index);
+  end;
+
+  for Index := 1 to ColumnCount do
+  begin
+    pbxEx.Canvas.MoveTo(ColumnStep * Index, 0);
+    pbxEx.Canvas.LineTo(ColumnStep * Index, pbxEx.Height);
+  end;
+
+  pbxEx.Canvas.Brush.Color := clBlack;
+
+  for RowIndex := 1 to RowCount do
+    for ColumnIndex := 1 to ColumnCount do
+    begin
+      if (ColumnIndex mod 2) = 0 then
+      begin
+        if (RowIndex mod 2) = 1 then
+          pbxEx.Canvas.FloodFill(ColumnStep * (ColumnIndex - 1) - pbxEx.Canvas.Pen.Width, RowIndex  * RowStep - 3, clBlack, fsBorder)
+        else
+          pbxEx.Canvas.FloodFill(ColumnStep * ColumnIndex - pbxEx.Canvas.Pen.Width, RowIndex  * RowStep - 3, clBlack, fsBorder);
+      end;
+    end;
+
+
+
+end;
+
+end.
