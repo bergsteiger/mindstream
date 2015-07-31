8920a46b45412d71532ffa3b13e27cc42c47a4f8
Ingword <instigator21@gmail.com>
2015-07-31 04:15:37 +0300

    Заготовочка

--- EmbaTasks/Module11/E6/Unit1.pas
+++ EmbaTasks/Module11/E6/Unit1.pas
@@ -41,21 +41,59 @@ end;
 
 procedure TfrmMain.Draw;
 var
-  Radius : integer;
-  LeftTop, RightBottom : TPoint;
+  Radius,
+  NPoints : integer;
+
+  LeftTop,
+  RightBottom,
+  CenterPoint,
+  CirclePoint,
+  StartPoint,
+  EndPoint : TPoint;
+
+  Angle : Double;
 begin
   pbxEx.Canvas.Pen.Color:= clBlack;
   pbxEx.Canvas.Pen.Width:= 2;
 
   Radius := StrToInt(edtRadius.Text);
-  LeftTop.X := StrToInt(edtCenterX.Text) - Radius;
-  LeftTop.Y := StrToInt(edtCenterY.Text) - Radius;
+  NPoints := StrToInt(edtNPoints.Text);
+  CenterPoint.X := StrToInt(edtCenterX.Text);
+  CenterPoint.Y := StrToInt(edtCenterY.Text);
 
+  LeftTop.X := CenterPoint.X - Radius;
+  LeftTop.Y := CenterPoint.Y - Radius;
   RightBottom.X := StrToInt(edtCenterX.Text) + Radius;
   RightBottom.Y := StrToInt(edtCenterY.Text) + Radius;
 
-  pbxEx.Canvas.Ellipse(LeftTop.X, LeftTop.Y,
-                       RightBottom.X, RightBottom.Y);
+  StartPoint.X := CenterPoint.X - Radius;
+  StartPoint.Y := CenterPoint.Y;
+
+  Angle := 0;
+  while Angle < (2 * PI) do
+  begin
+    EndPoint.X := CenterPoint.X + Round(Radius * cos(Angle));
+    EndPoint.Y := CenterPoint.Y - Round(Radius * sin(Angle));
+
+    pbxEx.Canvas.Pen.Color:= clBlack;
+    pbxEx.Canvas.ArcTo(LeftTop.X, LeftTop.Y,
+                       RightBottom.X, RightBottom.Y,
+                       StartPoint.X, StartPoint.Y,
+                       EndPoint.X, EndPoint.Y);
+
+
+    pbxEx.Canvas.Pen.Color:= clRed;
+    pbxEx.Canvas.MoveTo(EndPoint.X, EndPoint.Y);
+    pbxEx.Canvas.LineTo(EndPoint.X + 1, EndPoint.Y + 1);
+
+    StartPoint.X := EndPoint.X;
+    StartPoint.Y := EndPoint.Y;
+    Angle := Angle + ((2 * PI) / NPoints);
+  end;
+
+  pbxEx.Canvas.Pen.Color:= clBlack;
+  pbxEx.Canvas.MoveTo(StrToInt(edtCenterX.Text), StrToInt(edtCenterY.Text));
+  pbxEx.Canvas.LineTo(StrToInt(edtCenterX.Text)+1, StrToInt(edtCenterY.Text)+1);
 end;
 
 
