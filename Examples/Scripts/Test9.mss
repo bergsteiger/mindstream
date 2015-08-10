86973570e5e9db3a991a82b0ceb84ed8237df024
Alex W. Lulin <lulinalex@gmail.com>
2015-08-10 23:24:45 +0300

    - правим пути.

---
+++ Examples/Scripts/Test9.mss
@@ -0,0 +1,1 @@
+@ . '*.mss.co' '.' ProcessFilesWithMask
--- Examples/Scripts/callalltoout.cmd
+++ Examples/Scripts/callalltoout.cmd
@@ -1,3 +1,3 @@
 call clear.cmd
 del *.out
-for %%i in ( *.script ) do callMSS.exe %%i >> %%i.out
+for %%i in ( *.mss ) do callMSS.exe %%i >> %%i.out
