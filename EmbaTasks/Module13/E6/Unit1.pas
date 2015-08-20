332b910da369618d89f9fdc8e6de01e2ad73f1ff
Ingword <instigator21@gmail.com>
2015-08-20 16:48:45 +0300

    Заготовка под E6

---
+++ EmbaTasks/Module13/E6/Unit1.pas
@@ -0,0 +1,59 @@
+unit Unit1;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls;
+
+type
+  TForm1 = class(TForm)
+    edtIn: TEdit;
+    edtOut: TEdit;
+    btnDoIt: TButton;
+    procedure btnDoItClick(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+procedure TForm1.btnDoItClick(Sender: TObject);
+var
+  InStr, NumberStr, AvgNumberStr, NewSubStr : string;
+  Index, BeginPos, EndPos, SumNumber : Integer;
+  Average : double;
+begin
+  InStr := edtIn.Text;
+
+  BeginPos := Pos('(', InStr);
+  EndPos := Pos(')', InStr);
+
+  NumberStr := Copy(InStr, BeginPos + 1, EndPos - BeginPos - 1);
+
+  edtOut.Text := NumberStr;
+
+  SumNumber := 0;
+  for Index := 1 to Length(NumberStr) do
+    SumNumber := SumNumber + StrToInt(NumberStr[Index]);
+
+  Average := SumNumber / Length(NumberStr);
+  Str(Average :6 :2, AvgNumberStr);
+
+  NewSubStr := Copy(InStr, 0, BeginPos);
+  for Index := 1 to Length(NumberStr) do
+    NewSubStr := NewSubStr + AvgNumberStr;
+
+  NewSubStr := NewSubStr + Copy(InStr, EndPos, Length(InStr) - EndPos + 1);
+
+  edtOut.Text := NewSubStr;
+end;
+
+end.
