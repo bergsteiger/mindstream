50a2835bfeb71b8350124be43922fe642f6a2aea
Alex W. Lulin <lulinalex@gmail.com>
2015-08-21 04:41:38 +0300

    - используем генератор pas.

---
+++ Examples/Scripts/CodeGeneration/CodeGen57.ms.script.Project1.pas
@@ -0,0 +1,28 @@
+pas : <<Project>> Project1
+ pas : <<Library>> Library1
+ ; // Library1
+ pas : <<Library>> Library2
+ ; // Library2
+ pas : <<Library>> Library3
+ ; // Library3
+ pas : <<Programm>> Programm1
+  pas : <<Class>> Class1
+  ; // Class1
+  pas : <<Class>> Class2
+   pas : %INHERITS %G
+   ; // %G
+   pas : %IMPLEMENTS %R
+   ; // %R
+  ; // Class2
+  pas : <<Class>> Class3
+  ; // Class3
+  pas : <<Class>> Class4
+   pas : %INHERITS %G
+   ; // %G
+  ; // Class4
+  pas : <<Interface>> Interface1
+  ; // Interface1
+  pas : <<Interface>> Interface2
+  ; // Interface2
+ ; // Programm1
+; // Project1
