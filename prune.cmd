bd61ee61b358645082a7532f7fd43ee8b66008e9
Alex W. Lulin <lulinalex@gmail.com>
2015-09-04 00:24:25 +0300

    - добавляем файл для очистки мусора.

---
+++ prune.cmd
@@ -0,0 +1,11 @@
+pushd h:\
+del *.bak *.~* *.*~ *.tmp .#* debug.jpg /s
+popd
+
+rem pushd h:\Dropbox\
+rem del *.bak *.~* *.*~ *.tmp .#* debug.jpg /s
+rem popd
+
+rem pushd g:\
+rem del *.diff.log *.sdiff.log *.log.uni /s
+rem popd
