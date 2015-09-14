ce88e929009dc8e5b267d59e9368f820394a3e95
Alex W. Lulin <lulinalex@gmail.com>
2015-09-14 14:38:27 +0300

    - архивируем.

--- EmbaTasks/arc.cmd
+++ EmbaTasks/arc.cmd
@@ -1,2 +1,2 @@
 call clean.cmd
-"C:\'utils\Rar.exe" a -x*.dcu -x*.~*~ -x*.exe -xarc.cmd -x*.gitignore -x__history -r arc
+"C:\'utils\Rar.exe" a -x*.dcu -x*.~*~ -x*.exe -xarc.cmd -x*.gitignore -x__history -r -x*.pdf arc
