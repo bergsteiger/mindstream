4949ade032e452f89b21b8cb9e65820873ccac1b
Alex W. Lulin <lulinalex@gmail.com>
2015-09-22 22:48:53 +0300

    - заготовочка для #1159.

--- Examples/Protocols/Protocols.dpr
+++ Examples/Protocols/Protocols.dpr
@@ -7,9 +7,21 @@ program Protocols;
 uses
   System.SysUtils;
 
+type
+ TmyClass = class
+
+ end;//TmyClass
+
+var
+ l_C : TmyClass;
 begin
   try
-    { TODO -oUser -cConsole Main : Insert code here }
+    l_C := TmyClass.Create;
+    try
+
+    finally
+      FreeAndNil(l_C);
+    end;
   except
     on E: Exception do
       Writeln(E.ClassName, ': ', E.Message);
