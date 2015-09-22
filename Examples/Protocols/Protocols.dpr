dc69a6afa1f7df600f258d4e63883c33869ca5e2
Alex W. Lulin <lulinalex@gmail.com>
2015-09-22 22:54:28 +0300

    - заготовочка для #1159.

--- Examples/Protocols/Protocols.dpr
+++ Examples/Protocols/Protocols.dpr
@@ -9,8 +9,20 @@ uses
 
 type
  TmyClass = class
-
+  public
+   procedure ForRegularUser1;
+   procedure ForRegularUser2;
  end;//TmyClass
+
+procedure TmyClass.ForRegularUser1;
+begin
+  WriteLn('ForRegularUser1');
+end;
+
+procedure TmyClass.ForRegularUser2;
+begin
+  WriteLn('ForRegularUser2');
+end;
 
 var
  l_C : TmyClass;
@@ -18,7 +30,8 @@ begin
   try
     l_C := TmyClass.Create;
     try
-
+      l_C.ForRegularUser1;
+      l_C.ForRegularUser2;
     finally
       FreeAndNil(l_C);
     end;
