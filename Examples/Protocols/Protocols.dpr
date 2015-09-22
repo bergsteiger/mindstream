e2f57e9f71eb3ae830b3ab6dd0044fcc045b5d9c
Alex W. Lulin <lulinalex@gmail.com>
2015-09-22 23:09:04 +0300

    - заготовочка для #1159.

--- Examples/Protocols/Protocols.dpr
+++ Examples/Protocols/Protocols.dpr
@@ -10,12 +10,42 @@ uses
 type
  TmyClass = class
   public
+   type
+    Advanced1 = record
+     private
+      f_Provider : TmyClass;
+     public
+      constructor Create(aProvider: TmyClass);
+      procedure ForAdvancedUser1;
+      procedure ForAdvancedUser2;
+    end;//Advanced1
+  private
    procedure ForAdvancedUser1;
    procedure ForAdvancedUser2;
   public
    procedure ForRegularUser1;
    procedure ForRegularUser2;
+   function AsA1: Advanced1;
  end;//TmyClass
+
+// TmyClass.Advanced1
+
+constructor TmyClass.Advanced1.Create(aProvider: TmyClass);
+begin
+ f_Provider := aProvider;
+end;
+
+procedure TmyClass.Advanced1.ForAdvancedUser1;
+begin
+ f_Provider.ForAdvancedUser1;
+end;
+
+procedure TmyClass.Advanced1.ForAdvancedUser2;
+begin
+ f_Provider.ForAdvancedUser2;
+end;
+
+// TmyClass
 
 procedure TmyClass.ForAdvancedUser1;
 begin
@@ -37,6 +67,11 @@ begin
   WriteLn('ForRegularUser2');
 end;
 
+function TmyClass.AsA1: Advanced1;
+begin
+  Result := Advanced1.Create(Self);
+end;
+
 var
  l_C : TmyClass;
 begin
@@ -45,6 +80,8 @@ begin
     try
       l_C.ForRegularUser1;
       l_C.ForRegularUser2;
+      l_C.AsA1.ForAdvancedUser1;
+      l_C.AsA1.ForAdvancedUser2;
     finally
       FreeAndNil(l_C);
     end;
