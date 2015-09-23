78ac732df5a78823891330fa2b0968dabd1ded91
Ingword <instigator21@gmail.com>
2015-09-23 18:12:37 +0300

    Все ещё заготовка

--- EmbaTasks/Module20/E1/Unit1.dfm
+++ EmbaTasks/Module20/E1/Unit1.dfm
@@ -11,7 +11,6 @@ object fmMain: TfmMain
   Font.Name = 'Tahoma'
   Font.Style = []
   OldCreateOrder = False
-  OnCreate = FormCreate
   PixelsPerInch = 96
   TextHeight = 13
   object pnlTop: TPanel
@@ -21,31 +20,30 @@ object fmMain: TfmMain
     Height = 49
     Align = alTop
     TabOrder = 0
-    ExplicitWidth = 603
     object lbl3: TLabel
-      Left = 235
+      Left = 11
       Top = 16
-      Width = 76
+      Width = 72
       Height = 13
-      Caption = 'Scale intervals :'
+      Caption = 'Column count :'
     end
     object lbl4: TLabel
-      Left = 19
+      Left = 195
       Top = 16
-      Width = 119
+      Width = 58
       Height = 13
-      Caption = 'Number of experiments :'
+      Caption = 'Row count :'
     end
-    object edtNumExperiments: TEdit
-      Left = 144
+    object edtRowCount: TEdit
+      Left = 264
       Top = 13
       Width = 74
       Height = 21
       TabOrder = 1
       Text = '5'
     end
-    object edtScaleIntervals: TEdit
-      Left = 317
+    object edtColCount: TEdit
+      Left = 93
       Top = 13
       Width = 74
       Height = 21
@@ -53,22 +51,13 @@ object fmMain: TfmMain
       Text = '10'
     end
     object btnBuildGrid: TButton
-      Left = 429
+      Left = 365
       Top = 11
       Width = 103
       Height = 25
       Caption = 'Build grid'
       TabOrder = 0
       OnClick = btnBuildGridClick
-    end
-    object btnCalculateResult: TButton
-      Left = 557
-      Top = 11
-      Width = 103
-      Height = 25
-      Caption = 'Calculate Result'
-      TabOrder = 3
-      OnClick = btnCalculateResultClick
     end
   end
   object pnlMain: TPanel
@@ -79,19 +68,18 @@ object fmMain: TfmMain
     Align = alClient
     Caption = 'pnlMain'
     TabOrder = 1
-    ExplicitWidth = 603
     object sgdMy: TStringGrid
       Left = 1
       Top = 1
       Width = 704
       Height = 231
       Align = alClient
-      Color = clBtnFace
+      Color = clMoneyGreen
       FixedCols = 0
       RowCount = 2
+      FixedRows = 0
       Options = [goFixedVertLine, goFixedHorzLine, goHorzLine, goRangeSelect, goRowSizing, goColSizing, goEditing]
       TabOrder = 0
-      ExplicitWidth = 601
     end
   end
 end
--- EmbaTasks/Module20/E1/Unit1.pas
+++ EmbaTasks/Module20/E1/Unit1.pas
@@ -14,13 +14,10 @@ type
     sgdMy: TStringGrid;
     lbl3: TLabel;
     lbl4: TLabel;
-    edtNumExperiments: TEdit;
-    edtScaleIntervals: TEdit;
+    edtRowCount: TEdit;
+    edtColCount: TEdit;
     btnBuildGrid: TButton;
-    btnCalculateResult: TButton;
-    procedure FormCreate(Sender: TObject);
     procedure btnBuildGridClick(Sender: TObject);
-    procedure btnCalculateResultClick(Sender: TObject);
   private
     { Private declarations }
   public
@@ -36,51 +33,23 @@ implementation
 
 procedure TfmMain.btnBuildGridClick(Sender: TObject);
 var
-  i : integer;
+  i, j, RowCount, ColCount: integer;
 begin
-  sgdMy.RowCount := StrToInt(edtNumExperiments.Text) + 1;
-  for i := 1 to sgdMy.RowCount do
-  begin
-    sgdMy.Cells[0, i] := IntToStr(i);
-    sgdMy.Cells[1, i] := edtScaleIntervals.Text;
-  end;
-end;
+  sgdMy.ColCount := StrToInt(edtColCount.Text);
+  sgdMy.RowCount := StrToInt(edtRowCount.Text);
 
-procedure TfmMain.btnCalculateResultClick(Sender: TObject);
-var
-  i, MaxRow, MinRow : integer;
-begin
-  MaxRow := 1;
-  MinRow := 1;
+  sgdMy.Cells[1,1] := 'qwe';
+{  n := StrToInt(edit1.Text);
+  m := StrToInt(edit2.Text);
+  stringgrid1.RowCount := n;
+  stringgrid1.ColCount := m;
 
-  for i := 1 to sgdMy.RowCount - 1 do
-  begin
-    sgdMy.Cells[3, i] := IntToStr(StrToInt(sgdMy.Cells[2, i]) *
-                                  StrToInt(edtScaleIntervals.Text));
-
-    if StrToInt(sgdMy.Cells[3, i]) > StrToInt(sgdMy.Cells[3, MaxRow]) then
-      MaxRow := i;
-
-    if StrToInt(sgdMy.Cells[3, i]) < StrToInt(sgdMy.Cells[3, MinRow]) then
-      MinRow := i;
-  end;
-
-  sgdMy.Cells[4, MaxRow] := 'Max Value';
-  sgdMy.Cells[4, MinRow] := 'Min Value';
-end;
-
-procedure TfmMain.FormCreate(Sender: TObject);
-begin
-  sgdMy.Cells[0, 0] := 'Number of Experiments';
-  sgdMy.ColWidths[0] := 120;
-  sgdMy.Cells[1, 0] := 'Scale Intervals';
-  sgdMy.ColWidths[1] := 85;
-  sgdMy.Cells[2, 0] := 'Measured Values';
-  sgdMy.ColWidths[2] := 90;
-  sgdMy.Cells[3, 0] := 'Result';
-  sgdMy.ColWidths[3] := 40;
-  sgdMy.Cells[4, 0] := 'Max/Min';
-  sgdMy.ColWidths[4] := 70;
+  for i := 0 to n - 1 do
+    for j := 0 to m - 1 do
+      if i mod 2 = 0 then
+        stringgrid1.Cells[j, i] := IntToStr(i * m + j + 1)
+      else
+        stringgrid1.Cells[j, i] := IntToStr((i + 1) * m - j)   }
 end;
 
 end.
