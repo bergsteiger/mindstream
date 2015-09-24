983a3cea918eb5ef51d6d7d7ee6326d65a994755
Ingword <instigator21@gmail.com>
2015-09-24 17:59:21 +0300

    Заготовка 22.2

---
+++ EmbaTasks/Module22/E2/Unit1.dfm
@@ -0,0 +1,85 @@
+object fmMain: TfmMain
+  Left = 0
+  Top = 0
+  Caption = 'Main'
+  ClientHeight = 554
+  ClientWidth = 706
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'Tahoma'
+  Font.Style = []
+  OldCreateOrder = False
+  PixelsPerInch = 96
+  TextHeight = 13
+  object pnlTop: TPanel
+    Left = 0
+    Top = 0
+    Width = 706
+    Height = 73
+    Align = alTop
+    TabOrder = 0
+    object lblHighest: TLabel
+      Left = 21
+      Top = 16
+      Width = 43
+      Height = 13
+      Caption = 'Highest :'
+    end
+    object lblLowlest: TLabel
+      Left = 190
+      Top = 16
+      Width = 43
+      Height = 13
+      Caption = 'Lowlest :'
+    end
+    object btnBuildGrid: TButton
+      Left = 182
+      Top = 40
+      Width = 343
+      Height = 25
+      Caption = 'Build grid'
+      TabOrder = 0
+      OnClick = btnBuildGridClick
+    end
+    object edtHighest: TEdit
+      Left = 247
+      Top = 13
+      Width = 74
+      Height = 21
+      TabOrder = 1
+      Text = '-50'
+    end
+    object edtLowlest: TEdit
+      Left = 76
+      Top = 13
+      Width = 74
+      Height = 21
+      TabOrder = 2
+      Text = '50'
+    end
+  end
+  object pnlMain: TPanel
+    Left = 0
+    Top = 73
+    Width = 706
+    Height = 481
+    Align = alClient
+    Caption = 'pnlMain'
+    TabOrder = 1
+    object sgdMy: TStringGrid
+      Left = 1
+      Top = 1
+      Width = 704
+      Height = 479
+      Align = alClient
+      Color = clMoneyGreen
+      FixedCols = 0
+      RowCount = 2
+      FixedRows = 0
+      Options = [goFixedVertLine, goFixedHorzLine, goHorzLine, goRangeSelect, goRowSizing, goColSizing, goEditing]
+      TabOrder = 0
+    end
+  end
+end
---
+++ EmbaTasks/Module22/E2/Unit1.pas
@@ -0,0 +1,85 @@
+unit Unit1;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants,
+  System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Vcl.Grids, Vcl.ExtCtrls;
+
+const
+  c_ColCount = 7;
+  c_RowCount = 12;
+
+type
+  TfmMain = class(TForm)
+    pnlTop: TPanel;
+    pnlMain: TPanel;
+    sgdMy: TStringGrid;
+    btnBuildGrid: TButton;
+    lblHighest: TLabel;
+    lblLowlest: TLabel;
+    edtHighest: TEdit;
+    edtLowlest: TEdit;
+    procedure btnBuildGridClick(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  fmMain: TfmMain;
+
+implementation
+
+{$R *.dfm}
+
+procedure TfmMain.btnBuildGridClick(Sender: TObject);
+var
+  i, j,
+  RowCount, ColCount : Integer;
+
+  RandomValue, SumEachRow : real;
+  ArrayOfReal : array[1 .. c_RowCount, 1 .. c_ColCount] of real;
+begin
+  ColCount := c_ColCount + 1;
+  RowCount := c_RowCount + 1;
+
+  sgdMy.ColCount := ColCount;
+  sgdMy.RowCount := RowCount;
+
+  sgdMy.FixedCols := 1;
+  sgdMy.FixedRows := 1;
+
+  for i := 1 to ColCount - 1 do
+   sgdMy.Cells[i, 0] := IntToStr(i);
+
+  for i := 1 to RowCount - 1 do
+    sgdMy.Cells[0, i] := IntToStr(i);
+
+  for i := 1 to RowCount - 1 do
+    for j := 1 to ColCount - 1 do
+    begin
+      RandomValue := random * (StrToInt(edtLowlest.Text) -
+                               StrToInt(edtHighest.Text)) + StrToInt(edtHighest.Text);
+
+      ArrayOfReal[i, j] := RandomValue;
+    end;
+
+  sgdMy.ColCount := sgdMy.ColCount + 1;
+  sgdMy.Cells[sgdMy.ColCount - 1, 0] := ' Sum ';
+
+  for i := 1 to RowCount - 1 do
+  begin
+    SumEachRow := 0;
+    for j := 1 to ColCount - 1 do
+    begin
+      sgdMy.Cells[j, i] := FloatToStr(ArrayOfReal[i, j]);
+      SumEachRow := SumEachRow + (ArrayOfReal[i, j]);
+    end;
+    sgdMy.Cells[sgdMy.ColCount - 1, i] := FloatToStr(SumEachRow);
+  end;
+end;
+
+end.
