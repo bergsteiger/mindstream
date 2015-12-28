021ddb50aae74c3778db56949b15cec36d4d89f4
Ingword <instigator21@gmail.com>
2015-12-28 23:10:38 +0200

    Добавляем новый парсер, заготовку

---
+++ Parser/uNewParser.pas
@@ -0,0 +1,62 @@
+unit uNewParser;
+
+interface
+
+uses
+  Classes;
+
+type
+  TscriptTokenType = (ttUnknown, ttString);
+
+type
+  TNewParser = class
+  private
+    f_Stream: TStringStream;
+    f_EOF: Boolean;
+    f_CurrentLine: String;
+    f_PosInCurrentLine: integer;
+  public
+    constructor Create(const aStream: TStringStream); overload;
+    constructor Create(const aFileName: String); overload;
+    destructor Destroy; override;
+    function EOF: Boolean;
+    { * - Достигнут конец входного потока. }
+  end; // TNewParser
+
+implementation
+
+uses
+  System.SysUtils;
+
+{ TNewParser }
+
+constructor TNewParser.Create(const aStream: TStringStream);
+begin
+  inherited Create;
+  f_PosInCurrentLine := 1;
+  f_EOF := false;
+  f_Stream := aStream;
+end;
+
+constructor TNewParser.Create(const aFileName: String);
+var
+  l_FileName: String;
+begin
+  l_FileName := aFileName;
+  if (ExtractFilePath(l_FileName) = '') then
+    l_FileName := ExtractFilePath(ParamStr(0)) + '\' + l_FileName;
+  Create(TStringStream.Create(l_FileName, fmOpenRead));
+end;
+
+destructor TNewParser.Destroy;
+begin
+  FreeAndNil(f_Stream);
+  inherited;
+end;
+
+function TNewParser.EOF: Boolean;
+begin
+  Result := f_EOF AND (f_CurrentLine = '');
+end;
+
+end.
