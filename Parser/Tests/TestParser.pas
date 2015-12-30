dc4a9c771c2d99dd039701b8f4ca3fc6c53283ba
Ingword <instigator21@gmail.com>
2015-12-30 14:45:03 +0200

    Заготовка

--- Parser/Tests/TestParser.pas
+++ Parser/Tests/TestParser.pas
@@ -12,7 +12,11 @@ uses
   // https://bitbucket.org/lulinalex/mindstream/wiki/%D0%A1%D1%82%D0%B0%D1%82%D1%8C%D0%B8%20%D0%BD%D0%B0%20%D1%80%D1%83%D1%81%D1%81%D0%BA%D0%BE%D0%BC/%D0%A3%D1%81%D1%82%D1%80%D0%BE%D0%B9%D1%81%D1%82%D0%B2%D0%BE%20%D1%81%D0%BA%D1%80%D0%B8%D0%BF%D1%82%D0%BE%D0%B2%D0%BE%D0%B9%20%D0%BC%D0%B0%D1%88%D0%B8%D0%BD%D1%8B/%D0%9E%D0%BF%D0%B8%D1%81%D0%B0%D0%BD%D0%B8%D0%B5%20%D1%84%D0%BE%D1%80%D0%BC%D0%B0%D1%82%D0%B0%20%D1%81%D0%BA%D1%80%D0%B8%D0%BF%D1%82%D0%BE%D0%B2
 
 type
+  TSomeProcedure = reference to procedure(aParser: TScriptParser);
+
   TestTParser = class(TTestCase)
+  private
+    function DoIt(const aFileName: String; const aLambda: TSomeProcedure): boolean;
   published
     procedure TestCreate;
     procedure FirstTest;
@@ -28,47 +32,53 @@ type
 
 implementation
 
+function TestTParser.DoIt(const aFileName: String;
+  const aLambda: TSomeProcedure): boolean;
+var
+  l_Parser : TScriptParser;
+begin
+  l_Parser := TScriptParser.Create(aFileName);
+  try
+    aLambda(l_Parser);
+  finally
+    FreeAndNil(l_Parser);
+  end;
+end;
+
 procedure TestTParser.FirstTest;
 begin
   Check(True);
 end;
 
 procedure TestTParser.TestCreate;
-var
-  l_Parser : TScriptParser;
 begin
-  l_Parser := TScriptParser.Create('Test_4_1.txt');
-
-  CheckFalse(l_Parser.EOF);
-
-  FreeAndNil(l_Parser);
+  doIt('Test_4_1.txt',
+    procedure (aParser : TScriptParser)
+    begin
+      CheckFalse(aParser.EOF);
+    end);
 end;
 
 procedure TestTParser.Test_4_1;
-var
-  l_Parser : TScriptParser;
-  l_Char : AnsiChar;
 begin
-  l_Parser := TScriptParser.Create('Test_4_1.txt');
+  doIt(Name + '.txt',
+    procedure (aParser : TScriptParser)
+    begin
+      aParser.NextToken;
 
-  try
-    l_Parser.NextToken;
+      CheckTrue((aParser.TokenString = 'A') and
+                (aParser.TokenType = ttToken));
 
-    CheckTrue((l_Parser.TokenString = 'A') and
-              (l_Parser.TokenType = ttToken));
+      aParser.NextToken;
 
-    l_Parser.NextToken;
+      CheckTrue((aParser.TokenString = 'B') and
+                (aParser.TokenType = ttToken));
 
-    CheckTrue((l_Parser.TokenString = 'B') and
-              (l_Parser.TokenType = ttToken));
+      aParser.NextToken;
 
-    l_Parser.NextToken;
-
-    CheckTrue((l_Parser.TokenString = 'C') and
-              (l_Parser.TokenType = ttToken));
-  finally
-    FreeAndNil(l_Parser);
-  end;
+      CheckTrue((aParser.TokenString = 'C') and
+                (aParser.TokenType = ttToken));
+    end);
 end;
 
 procedure TestTParser.Test_4_2;
@@ -217,6 +227,7 @@ begin
     FreeAndNil(l_Parser);
   end;
 end;
+
 
 initialization
   // Register any test cases with the test runner
