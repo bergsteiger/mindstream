ca1e45c77007e4618321944d8dd9a2b877c196a7
Ingword <instigator21@gmail.com>
2016-02-20 21:53:18 +0200

    add msLog and UnEqualTokens.txt

--- Parser/Parser.dpr
+++ Parser/Parser.dpr
@@ -8,7 +8,8 @@ uses
   uNewParser in 'uNewParser.pas',
   l3Parser in '..\l3Parser\l3Parser.pas',
   uL3ParserVsNewParser in 'uL3ParserVsNewParser.pas',
-  uTypes in 'uTypes.pas';
+  uTypes in 'uTypes.pas',
+  msLog in 'msLog.pas';
 
 {$R *.res}
 
